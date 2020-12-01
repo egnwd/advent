b250cc5fd93b901cec26d9326ac00fedbae47f07
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-01 14:49:02 +0000

    ⭐ Day 1 - Part 2

--- execs/Day01.hs
+++ execs/Day01.hs
@@ -5,12 +5,12 @@
 module Main (main) where
 
 import Advent
-import Text.Megaparsec
 
 main :: IO ()
 main = do
   input <- getParsedLines 1 number
   print $ expenseReport input
+  print $ expenseReport3 input
 
 -- | Find two numbers in list that sum to 2020, then multiply the answers
 --
@@ -18,3 +18,10 @@ main = do
 -- 514579
 expenseReport :: [Integer] -> Integer
 expenseReport ns = head [ x * y | x <- ns, y <- ns, let k = x + y, k == 2020]
+
+-- | Find three numbers in list that sum to 2020, then multiply the answers
+--
+-- >>> expenseReport3 [1721, 979, 366, 299, 675, 1456]
+-- 241861950
+expenseReport3 :: [Integer] -> Integer
+expenseReport3 ns = head [ x * y * z | x <- ns, y <- ns, z <- ns, let k = x + y + z, k == 2020]
