8ce05351c3c3b536176a04e1d7add6dfcc2be79d
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-01 14:46:05 +0000

    ⭐ Day 1 - Part 1

--- execs/Day01.hs
+++ execs/Day01.hs
@@ -1,6 +1,20 @@
-module Main where
+{-|
+   Name: Expense Reports
+   Url: <https://adventofcode.com/2020/day/1>
+-}
+module Main (main) where
 
 import Advent
+import Text.Megaparsec
 
 main :: IO ()
-main = someFunc
+main = do
+  input <- getParsedLines 1 number
+  print $ expenseReport input
+
+-- | Find two numbers in list that sum to 2020, then multiply the answers
+--
+-- >>> expenseReport [1721, 979, 366, 299, 675, 1456]
+-- 514579
+expenseReport :: [Integer] -> Integer
+expenseReport ns = head [ x * y | x <- ns, y <- ns, let k = x + y, k == 2020]
--- src/Advent.hs
+++ src/Advent.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Advent
-    ( someFunc
+    ( getParsedLines
+    , Parser (..)
+    , number
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Prelude hiding (readFile)
+import Data.Text hiding (empty)
+import Data.Text.IO (readFile)
+import System.FilePath
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void Text
+
+getRawInput :: Int -> IO Text
+getRawInput i = readFile ("inputs" </> "input" <> show i <.> "txt")
+
+getParsedLines :: Int -> Parser a -> IO [a]
+getParsedLines i p = do
+  input <- getRawInput i
+  either fail return (parseLines p input)
+
+parseLines :: Parser a -> Text -> Either String [a]
+parseLines p input =
+  case parse ((p `sepBy` newline) <* eof) "input" input of
+    Left  e -> Left (errorBundlePretty e)
+    Right a -> Right a
+
+sc      = L.space hspace1 empty empty
+lexeme :: Parser a -> Parser a
+lexeme  = L.lexeme sc
+integer = lexeme L.decimal
+number  = L.signed sc integer
