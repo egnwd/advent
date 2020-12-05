ccaa54b577e3f719ad2a34195d13d397a0795fca
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-05 09:12:15 +0000

    Read in as binary directly

--- execs/Day05.hs
+++ execs/Day05.hs
@@ -10,17 +10,36 @@ module Main
 import Advent
 import Prelude hiding (unlines)
 
+import Refined
+import Data.Finite
+import Data.Bits
+import Data.Maybe
 import Data.Semigroup
 import Data.List
-import Data.Word
-
-import Debug.Trace
-
-import Control.Monad.State
+import Control.Lens
+import Control.Monad
 
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Text.Megaparsec as MP
+
+type Binary = Refined (SizeEqualTo 10) [Finite 2]
+
+readBinary :: String -> Int
+readBinary = bitsToInt . unrefine . readListBits
+  where
+    bitsToInt :: [Finite 2] -> Int
+    bitsToInt = foldl' toBin 0 . zip [9,8..0]
+    readListBits :: String -> Binary
+    readListBits = fromJust . (refineFail <=< traverse (preview (to bin)))
+    bin :: Char -> Finite 2
+    bin 'L' = finite 0
+    bin 'R' = finite 1
+    bin 'F' = finite 0
+    bin 'B' = finite 1
+    toBin :: Int -> (Int, Finite 2) -> Int
+    toBin acc (_, 0) = acc
+    toBin acc (i, 1) = setBit acc i
 
 main :: IO ()
 main = do
@@ -28,39 +47,20 @@ main = do
   print $ part1 input
   print $ part2 input
 
-type Input  = [String]
+type Input  = [Int]
 type Output = Int
 
 letters = S.fromList $ [ 'L', 'R', 'F', 'B' ]
 
 -- | Parsing
-parseInput :: Parser String
-parseInput = T.unpack <$> MP.takeWhileP Nothing (`S.member` letters)
+parseInput :: Parser Int
+parseInput = readBinary . T.unpack <$> MP.takeWhileP Nothing (`S.member` letters)
+
 
 part1 :: Input -> Output
-part1 = getMax . foldMap (Max . seatId)
+part1 = getMax . foldMap (Max)
 
 part2 :: Input -> Output
-part2 input = let x    = sort . map (seatId) $ input
+part2 input = let x    = sort $ input
                   diff = zipWith (-) (drop 1 x) x
                in (+1) . snd . head $ filter ((2 ==) . fst) $ zip diff x
-
-seatId :: String -> Output
-seatId s = let (s', (r, _r)) = runState  (getRow s)  (0, 127)
-               (c, _c)       = execState (getCol s') (0, 7)
-            in fromIntegral r * 8 + fromIntegral c
-
-getRow :: String -> State (Word8, Word8) String
-getRow ('F':r) = modify lowerHalf >> getRow r
-getRow ('B':r) = modify upperHalf >> getRow r
-getRow r = return r
-
-getCol :: String -> State (Word8, Word8) String
-getCol ('L':r) = modify lowerHalf >> getCol r
-getCol ('R':r) = modify upperHalf >> getCol r
-getCol r = return r
-
-lowerHalf (l,u) = (l, u - halfway l u)
-upperHalf (l,u) = (l + halfway l u, u)
-
-halfway l u = ((u-l) `div` 2) + 1
