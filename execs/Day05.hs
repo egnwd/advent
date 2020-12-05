d275382bfb1e2c320a86f06a83d0b4d1193845bd
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-05 10:06:21 +0000

    Finite was fun, but this is simpler

--- execs/Day05.hs
+++ execs/Day05.hs
@@ -10,36 +10,22 @@ module Main
 import Advent
 import Prelude hiding (unlines)
 
-import Refined
-import Data.Finite
-import Data.Bits
-import Data.Maybe
 import Data.Semigroup
 import Data.List
-import Control.Lens
-import Control.Monad
 
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Text.Megaparsec as MP
 
-type Binary = Refined (SizeEqualTo 10) [Finite 2]
-
 readBinary :: String -> Int
-readBinary = bitsToInt . unrefine . readListBits
+readBinary = bitsToInt
   where
-    bitsToInt :: [Finite 2] -> Int
-    bitsToInt = foldl' toBin 0 . zip [9,8..0]
-    readListBits :: String -> Binary
-    readListBits = fromJust . (refineFail <=< traverse (preview (to bin)))
-    bin :: Char -> Finite 2
-    bin 'L' = finite 0
-    bin 'R' = finite 1
-    bin 'F' = finite 0
-    bin 'B' = finite 1
-    toBin :: Int -> (Int, Finite 2) -> Int
-    toBin acc (_, 0) = acc
-    toBin acc (i, 1) = setBit acc i
+    bitsToInt = foldl' toBin 0
+    bin 'L' = 0
+    bin 'R' = 1
+    bin 'F' = 0
+    bin 'B' = 1
+    toBin acc i = 2*acc+(bin i)
 
 main :: IO ()
 main = do
