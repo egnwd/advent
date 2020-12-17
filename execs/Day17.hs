06712a3db15d2b3175d2598128634505e7d05f59
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-17 18:57:40 +0000

    Use sets and maps

--- execs/Day17.hs
+++ execs/Day17.hs
@@ -9,31 +9,28 @@ import Advent
 import Control.Arrow ((&&&))
 import Control.Monad
 import Linear
-import Data.List
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import qualified Data.Set as S
+import qualified Data.Map as M
 
 main :: IO ()
 main = print . (part mkCube &&& part mkTessaract) =<< getParsedInput 17 parseInput
 
-parseInput :: Parser [V2 Int]
-parseInput = map fst . filter snd . concat . zipWith (\i xs -> zipWith (\j x -> (V2 j i, x)) [0..] xs) [0..]
+parseInput :: Parser (S.Set (V2 Int))
+parseInput = S.fromList . map fst . filter snd . concat . zipWith (\i xs -> zipWith (\j x -> (V2 j i, x)) [0..] xs) [0..]
   <$> ((`sepBy` newline) . many $ (True <$ char '#' <|> False <$ char '.'))
 
-part f = length . (!! 6) . iterate step . map f
+part f = length . (!! 6) . iterate step . S.map f
 
-step wld = do
-  (c, n) <- numNeighbours $ concatMap neighbours wld
-  guard $ rule (c `elem` wld) n
-  return c
+step wld =
+    M.keysSet . M.filterWithKey (rule . (`S.member` wld))
+  . M.unionsWith (+)
+  . S.map (M.fromSet (const 1) . S.fromList . neighbours)
+  $ wld
 
 rule True  n = n == 2 || n == 3
 rule False n = n == 3
-
-numNeighbours :: Ord a => [a] -> [(a, Int)]
-numNeighbours xs = do
-  x <- group $ sort xs
-  return (head x, length x)
 
 mkCube (V2 x y) = V3 x y 0
 mkTessaract = vector . mkCube
