c9e3245501ac3544d28161991575ea72eb95824c
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-18 16:38:20 +0000

    Clean up

--- execs/Day18.hs
+++ execs/Day18.hs
@@ -6,17 +6,16 @@
 module Day18 (main) where
 
 import Advent
-import Prelude hiding (unlines)
 import Text.Megaparsec
+import Control.Monad
 import Control.Monad.Combinators.Expr
 
 main :: IO ()
-main = do
-    print . sum =<< getParsedLines 18 (expr table1)
-    print . sum =<< getParsedLines 18 (expr table2)
+main = solve table1 >> solve table2
+  where solve = print . sum <=< getParsedLines 18 . expr
 
 expr t = makeExprParser (term t) t
 term t = parens (expr t) <|> number
 table1 = [ [ binary "+" (+), binary "*" (*) ] ]
 table2 = [ [ binary "+" (+)], [ binary "*" (*) ] ]
-binary  name f = InfixL  (f <$ symbol name)
+binary name f = InfixL (f <$ symbol name)
