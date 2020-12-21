f255d4e5b7e29d25bb6a9be262d7f15f7fa756c1
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-21 08:25:09 +0000

    Clean up day 20 a bit

--- execs/Day20.hs
+++ execs/Day20.hs
@@ -8,13 +8,9 @@ module Day20 (main) where
 
 import Advent
 import Control.Arrow ((&&&))
-import Control.Applicative (liftA2)
-import Control.Lens
 import Control.Monad
-import Data.Foldable
 import Data.Ix (range)
 import Data.List
-import Data.Monoid
 import Data.Semigroup
 import Linear.V2
 import Data.Maybe
@@ -23,13 +19,9 @@ import Text.Megaparsec.Char
 import qualified Data.IntMap as M
 import qualified Data.Map as MP
 import qualified Data.Set as S
-import Debug.Trace
 
 main :: IO ()
-main = do
-  input <- getParsedInput 20 parseInput
-  print $ part1 input
-  print $ part2 input
+main = print . (part1 &&& part2) =<< getParsedInput 20 parseInput
 
 newtype Tile a = Tile { unTile :: a } deriving (Eq, Ord, Functor)
 type PhotoTile = Tile [[Bool]]
@@ -60,11 +52,13 @@ parseTile :: Parser (Int, PhotoTile)
 parseTile = (,) <$> (symbol "Tile" *> number <* char ':' <* newline) <*> parseTile'
 parseTile' = Tile <$> some (True <$ char '#' <|> False <$ (char '.' <|> char ' ')) `sepBy` try (newline >> notFollowedBy newline)
 
+part1 :: Input -> Output
 part1 tiles = let photo = fromJust $ assembleMap tiles
                in MP.foldrWithKey (\k a acc -> if k `S.member` corners then fst a * acc else acc) 1 photo
 
-corners = let x = maxSize in S.fromList [(0,0), (0,x), (x,0), (x,x)]
+corners = S.fromList [(0,0), (0,maxSize), (maxSize,0), (maxSize,maxSize)]
 
+part2 :: Input -> Output
 part2 tiles = let photo = fromJust $ assembleMap tiles
                   stitchedPhoto = createPhoto photo
                   mx = getMax . foldMap (findMonsters . convert) $ mutations <*> [stitchedPhoto]
@@ -95,13 +89,15 @@ assembleMap tiles = listToMaybe $ assembleMap' allOptions (range ((0,0), (maxSiz
       assembleMap' opts' is photo'
 
 valid :: MP.Map (Int, Int) PhotoTile -> PhotoTile -> (Int, Int) -> MP.Map (Int, Int) (Int, Int) -> Bool
-valid _      _    (0,0) _ = True
-valid lookup tile (0,j) photo = (lookup MP.! (photo MP.! (0,j-1))) `isLeftOf` tile
-valid lookup tile (i,0) photo = (lookup MP.! (photo MP.! (i-1,0))) `isAbove` tile
-valid lookup tile (i,j) photo = (lookup MP.! (photo MP.! (i-1,j))) `isAbove` tile && (lookup MP.! (photo MP.! (i,j-1))) `isLeftOf` tile
+valid _      _    (0,0) _     = True
+valid lookup tile (0,j) photo = previous lookup photo (0,j-1) `isLeftOf` tile
+valid lookup tile (i,0) photo = previous lookup photo (i-1,0) `isAbove` tile
+valid lookup tile (i,j) photo = previous lookup photo (i-1,j) `isAbove` tile && previous lookup photo (i,j-1) `isLeftOf` tile
 
-t `isAbove`  t' = t ^. bottomRow == t' ^. topRow
-t `isLeftOf` t' = t ^. rightColumn == t' ^. leftColumn
+previous a b = (a MP.!) . (b MP.!)
+
+t `isAbove`  t' = bottomRow t == topRow t'
+t `isLeftOf` t' = rightColumn t == leftColumn t'
 
 -- Mutations (Rotations and Reflections) and the ability to apply them to tagged Tiles (i.e. (Int, Tile)
 allMutations = MP.fromList . concatMap mutate . M.toList
@@ -121,18 +117,10 @@ reflections = [id, flipH, flipV]
 flipH = fmap reverse
 flipV = (fmap . fmap) reverse
 
--- Lenses to fetch edges
-topRow :: Getter PhotoTile Pixels
-topRow = to (fmap head)
-
-bottomRow :: Getter PhotoTile Pixels
-bottomRow = to (fmap last)
-
-leftColumn :: Getter PhotoTile Pixels
-leftColumn = to ((fmap . fmap) head)
-
-rightColumn :: Getter PhotoTile Pixels
-rightColumn = to ((fmap .fmap) last)
+topRow = fmap head
+bottomRow = fmap last
+leftColumn = (fmap . fmap) head
+rightColumn = (fmap .fmap) last
 
 
 -- | Sea Monster
@@ -148,12 +136,12 @@ seaMonster = convert sm
 convert :: PhotoTile -> Tile (S.Set (V2 Int))
 convert (Tile grid) = Tile $ S.fromList [V2 i j | (i, rs) <- zip [0..] grid, (j, c) <- zip [0..] rs, c]
 
+totalPixels = 8 * 12
+
 findMonsters :: Tile (S.Set (V2 Int)) -> Max Int
-findMonsters photo = Max . getSum . foldMap f $ range (pure 0, pure 96)
+findMonsters photo = Max . length . filter f $ range (pure 0, pure totalPixels)
   where
-    f :: V2 Int -> Sum Int
+    f :: V2 Int -> Bool
     f dv = let currMonster = S.map (+dv) <$> seaMonster
                overlap = S.intersection <$> currMonster <*> photo
-            in if currMonster == overlap then Sum 1 else Sum 0
-
-
+            in currMonster == overlap
