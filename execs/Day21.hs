50108f70e3afb0b85169594bac77766f4584e009
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-22 00:03:26 +0000

    Day21 refactorings

--- execs/Day21.hs
+++ execs/Day21.hs
@@ -16,14 +16,27 @@ import Text.Megaparsec.Char
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Text as T
-import Debug.Trace
 
 main :: IO ()
 main = print . (part1 &&& part2) =<< getParsedLines 21 parseInput
 
 type Input  = [M.Map T.Text (S.Set T.Text)]
-type Output = Int
 
+part1 :: Input -> Int
+part1 input = let possibleIngredients = S.unions . M.elems . M.unionsWith S.intersection $ input
+                  allIngredients      = S.unions . M.elems . M.unionsWith S.union        $ input
+                  notPossible         = allIngredients S.\\ possibleIngredients
+               in getSum . foldMap (Sum . (S.size . (`S.intersection` notPossible)) . S.unions . M.elems) $ input
+
+part2 :: Input -> T.Text
+part2 input = let possibilities = sortOn (length . snd) . M.toList . M.unionsWith S.intersection $ input
+               in T.intercalate "," . M.elems . fromJust . listToMaybe $ pick possibilities S.empty M.empty
+
+pick [] _ assignments = return assignments
+pick ((k, ingr):rest) chosen assignments = do
+  i <- S.toList (ingr S.\\ chosen)
+  pick rest (S.insert i chosen) (M.insert k i assignments)
+--
 -- | Parsing
 parseInput :: Parser (M.Map T.Text (S.Set T.Text))
 parseInput = do
@@ -34,16 +47,3 @@ parseInput = do
 word :: Parser T.Text
 word = takeWhile1P Nothing isAlpha
 
-part1 input = let a = S.unions . M.elems . M.unionsWith S.intersection $ input
-                  b = S.unions . M.elems . M.unionsWith S.union $ input
-                  np = b S.\\ a
-               in getSum . foldMap (Sum . M.foldr (\s acc -> acc + S.size (s `S.intersection` np)) 0 . M.mapKeysWith S.union (const 0)) $ input
-
-part2 input = let a = M.toList . M.unionsWith S.intersection $ input
-                  b = sortOn (length . snd) a
-               in T.intercalate "," . M.elems . fromJust . listToMaybe $ pick b S.empty M.empty
-
-pick [] _ assignments = return assignments
-pick ((k, ingr):rest) chosen assignments = do
-  i <- S.toList (ingr S.\\ chosen)
-  pick rest (S.insert i chosen) (M.insert k i assignments)
