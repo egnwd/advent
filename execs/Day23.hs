d35324bd1d24d9b38635ff7daeac2d19c35582a4
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-23 17:07:40 +0000

    ⭐ Day 23 part 2, another one with ST monad... need to learn more

--- execs/Day23.hs
+++ execs/Day23.hs
@@ -6,50 +6,62 @@
 module Day23 (main) where
 
 import Control.Arrow ((&&&))
-import Control.Lens
-import Data.Monoid
-import Data.List.PointedList
-import qualified Data.List.PointedList.Circular as PL
-import Data.Maybe
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.Unboxed
+
+type Input = [Int]
+type Output = Int
 
 main :: IO ()
 main = print . (part1 &&& part2) $ parseInput
 
-type Input  = PointedList Int
-type Output = Int
+parseInput = [9,1,6,4,3,8,2,7,5]
 
--- | Parsing
-parseInput :: PointedList Int
-parseInput = fromJust . fromList $ [9,1,6,4,3,8,2,7,5]
-testInput = fromJust . fromList $ [3,8,9,1,2,5,4,6,7]
+mkArray :: [Int] -> ST s (STUArray s Int Int)
+mkArray xs = do
+  arr <- newArray_ (minimum xs, maximum xs)   -- init array the size of the range of input
+  arr <$ zipWithM_ (writeArray arr) xs (drop 1 $ cycle xs) -- cup i (index i) -> cup j (value at i)
+{-# INLINE mkArray #-}
 
-numbers :: Int -> [Int]
-numbers n = [n-1, n-2..1]++[9,8..n]
+move :: (MArray a e m, Enum e, Ix e, Show e) => a e e -> e -> m e
+move arr i = do
+  -- start: i -> t1 -> t2 -> t3 -> ...
+  t1 <- readArray arr i
+  t2 <- readArray arr t1
+  t3 <- readArray arr t2
+  i' <- readArray arr t3
+
+  (mn, mx) <- getBounds arr
+  let next n = if pred n < mn then mx else pred n
+      j = until (\a -> a /= t1 && a /= t2 && a /= t3) next (next i)
+  -- interest: j -> k
+  k <- readArray arr j
+  -- next: i -> i' -> ... -> j -> t1 -> t2 -> t3 -> k
+  writeArray arr t3 k
+  writeArray arr j t1
+  writeArray arr i i'
+
+  return i'
+
+mkNumber :: UArray Int Int -> (Int, Int) -> (Int, Int)
+mkNumber arr (i, acc) = let j = arr ! i in (j, acc * 10 + j)
+{-# INLINE mkNumber #-}
 
 part1 :: Input -> Output
-part1 = ans . (!! 100) . iterate move
+part1 input = runST $ do
+  cups <- mkArray input
+  foldM_ (const . move cups) (head input) [1..100]
+  cups' <- freeze cups
+  let (mn, mx) = bounds cups'
+      (_, n) = foldl (const . mkNumber cups') (1, 0) [mn .. mx-1]
+  return n
 
 part2 :: Input -> Output
-part2 input = ans2 . (!! 10000000) . iterate move $ input & suffix %~ (++[10..1000000])
-
-move :: PointedList Int -> PointedList Int
-move input = let n = input ^. focus
-                 top3 = take 3 (input ^. suffix ++ input ^. prefix)
-                 rest = unsafeDelete . unsafeDelete . unsafeDelete . PL.next $ input
-                 output = findNext rest n
-              in PL.next . unsafeFind n $ output & suffix %~ (top3++)
-
-unsafeDelete = fromJust . PL.delete
-unsafeFind n = fromJust . find n
-
-ans :: PointedList Int -> Int
-ans input = let lst = unsafeFind 1 input
-             in foldl1 (\acc n -> acc * 10 + n) (lst ^. suffix ++ lst ^. prefix)
-
-ans2 :: PointedList Int -> Int
-ans2 input = let lst = unsafeFind 1 input
-                 a = PL.next lst ^. focus
-                 b = PL.moveN 2 lst ^. focus
-              in a * b
-
-findNext input n = fromJust . getAlt . foldMap (Alt . (`find` input)) $ numbers n
+part2 input = runST $ do
+  cups <- mkArray (input ++ [10..1000000])
+  foldM_ (const . move cups) (head input) [1..10000000]
+  a <- readArray cups 1
+  b <- readArray cups a
+  return (a * b)
