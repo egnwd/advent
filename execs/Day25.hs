0ee9fba133ee433d86ba5c5cb7732c9342506974
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2020-12-25 07:55:21 +0000

    Faster version, search works better for finding loop size than mapping powModInt

--- execs/Day25.hs
+++ execs/Day25.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
 {-|
    Name: Combo Breaker
    Url: <https://adventofcode.com/2020/day/25>
@@ -7,6 +6,7 @@
 module Day25 (main) where
 
 import Advent
+import Math.NumberTheory.Powers.Modular
 
 subject = 7
 modNumber = 20201227
@@ -14,13 +14,6 @@ modNumber = 20201227
 main :: IO ()
 main = print . part1 =<< getParsedLines 25 number
 
-type Input  = [Int]
-type Output = Int
-
-part1 :: Input -> Output
 part1 [cardPublicKey, doorPublicKey] =
-  let cardLoopSize = fst . head . filter ((== cardPublicKey) . snd) . iterate (solve subject) $ (0, subject)
-   in snd . (!! cardLoopSize) . iterate (solve doorPublicKey) $ (0, doorPublicKey)
-
-solve subj (!i, !acc) = (i+1, (acc * subj) `rem` modNumber)
-{-# INLINE solve #-}
+  let cardLoopSize = fst . head . filter ((== cardPublicKey) . snd) . zip [1..] . iterate ((`rem` modNumber) . (*subject)) $ subject
+   in powModInt doorPublicKey cardLoopSize modNumber
