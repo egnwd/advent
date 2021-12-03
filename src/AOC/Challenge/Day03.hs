fd2800f831f90edcb8b22d2482e1e6ae01bd3a76
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2021-12-03 06:03:28 +0000

    💩 Messy day 3

--- src/AOC/Challenge/Day03.hs
+++ src/AOC/Challenge/Day03.hs
@@ -22,22 +22,56 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day03 (
-    -- day03a
-  -- , day03b
+    day03a
+  , day03b
   ) where
 
 import           AOC.Prelude
+import GHC.Float
 
-day03a :: _ :~> _
+parser = traverse (\x -> readMaybe [x] :: Maybe Int)
+
+solve xs = let a = solve' xs in [a, 1-a]
+solve' xs | countTrue (==1) xs >= countTrue (==0) xs = 1
+          | otherwise = 0
+
+toDec = sum . map(\(a,b) -> (2 ** a) * fromIntegral b) . zip [0..] . reverse
+
+solveb xs = double2Int $ toDec o * toDec c
+    where
+        o = loopEither oxygen (0, xs)
+        c = loopEither co2 (0, xs)
+
+oxygen (i, ns) = if length filtered == 1 then Left (head filtered) else Right (i+1, filtered)
+    where
+        common = map solve' (transpose ns)
+        filtered = filter ((==(common !! i)) . (!! i)) ns
+
+co2 (i, ns) = if length filtered == 1 then Left (head filtered) else Right (i+1, filtered)
+    where
+        common = map ((1-) . solve') (transpose ns)
+        filtered = filter ((==(common !! i)) . (!! i)) ns
+
+loopEither
+    :: (a -> Either r a)
+    -> a
+    -> r
+loopEither f = go
+  where
+    go !x = case f x of
+      Left  r  -> r
+      Right !y -> go y
+
+day03a :: _ :~> Int
 day03a = MkSol
-    { sParse = Just
+    { sParse = traverse parser . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . double2Int . product . map toDec . transpose . map (solve . sort) . transpose
     }
 
 day03b :: _ :~> _
 day03b = MkSol
-    { sParse = Just
+    { sParse = traverse parser . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solveb
     }
