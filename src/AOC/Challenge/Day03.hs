a7e30b0639d0ce585e3bd64a4b9326008052173c
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2021-12-06 21:44:27 +0000

    ♻️ Learn how to use recursion schemes (learn might be too strong)

--- src/AOC/Challenge/Day03.hs
+++ src/AOC/Challenge/Day03.hs
@@ -12,20 +12,23 @@ module AOC.Challenge.Day03 (
   , day03b
   ) where
 
-import AOC.Solver     ((:~>)(..))
-import AOC.Common     (loopEither)
-import Data.List      (transpose)
-import Data.Char      (digitToInt)
-import Data.Bifunctor (first, second)
-import Control.Lens   (each, (%~))
-import Data.Finite
+import AOC.Solver            ((:~>)(..))
+import Data.List             (transpose)
+import Data.Char             (digitToInt)
+import Data.Bifunctor        (first, second)
+import Control.Lens          (alaf)
+import Data.Monoid           (Product(Product))
+import Data.Finite           (Finite, finite, packFinite)
+import Data.Functor.Foldable (ListF(..), apo)
+import Control.Arrow         ((&&&))
+import Linear.V2             (V2(V2))
 
 type Bit = Finite 2
 type Bits = [Bit]
 
 zero, one :: Bit
 zero = finite 0
-one = finite 1
+one  = finite 1
 
 parser :: String -> Maybe [Bits]
 parser = traverse (traverse (packFinite . fromIntegral . digitToInt)) . lines
@@ -35,36 +38,39 @@ mostAndLeastCommonBit bs = let b = mostCommonBit bs in (b, 1-b)
 
 leastCommonBit, mostCommonBit :: Bits -> Bit
 leastCommonBit = (1-) . mostCommonBit
-mostCommonBit bs = let (zeros, ones) = foldr go (0,0) bs
-                    in if ones >= zeros then one else zero
+mostCommonBit bs = if ones >= zeros then one else zero
     where
+        (zeros, ones) = foldr go (0,0) bs
         go :: Bit -> (Int, Int) -> (Int, Int)
         go 0 = first succ
         go 1 = second succ
+        go _ = id
 
 binToDec :: Bits -> Int
 binToDec = foldl (\a -> (+) (2*a) . fromEnum . (== 1)) 0
 
+binaryProduct :: (Bits, Bits) -> Int
+binaryProduct = alaf Product foldMap binToDec . uncurry V2
+
 solvea :: [Bits] -> Int
-solvea xs = let (γ, ε) = (each %~ binToDec) . unzip . map mostAndLeastCommonBit . transpose $ xs
-             in γ * ε
+solvea = binaryProduct . unzip . map mostAndLeastCommonBit . transpose
 
 solveb :: [Bits] -> Int
-solveb bs = o * c
-    where
-        o = binToDec $ loopEither oxygen (0, bs)
-        c = binToDec $ loopEither co2 (0, bs)
+solveb = binaryProduct . (apo oxygen &&& apo co2)
 
-oxygen, co2 :: (Int, [Bits]) -> Either Bits (Int, [Bits])
+oxygen, co2 :: [Bits] -> ListF Bit (Either Bits [Bits])
 oxygen = criteria mostCommonBit
-co2 = criteria leastCommonBit
+co2    = criteria leastCommonBit
 
-criteria :: (Bits -> Bit) -> (Int, [Bits]) -> Either Bits (Int, [Bits])
-criteria f (i, xs)
-  = let b = (f . map (!! i) $ xs)
-     in case filter ((== b) . (!! i)) xs of
-          [n] -> Left n
-          ns  -> Right (succ i, ns)
+criteria :: (Bits -> Bit) -> [Bits] -> ListF Bit (Either Bits [Bits])
+criteria f bs = go
+    where
+        b = (f . map head) bs
+        ns = filter ((==b) . head) bs
+        go = case ns of
+              []  -> Nil
+              [n] -> Cons b (Left (tail n))
+              _   -> Cons b (Right (map tail ns))
 
 day03a :: [Bits] :~> Int
 day03a = MkSol
@@ -73,7 +79,7 @@ day03a = MkSol
     , sSolve = Just . solvea
     }
 
-day03b :: [Bits] :~> Int
+day03b :: [Bits] :~> _
 day03b = MkSol
     { sParse = parser
     , sShow  = show
