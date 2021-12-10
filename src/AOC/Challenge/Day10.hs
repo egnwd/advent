83ebbb8adece6a94f970c5a09528b3def24cacab
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2021-12-10 22:56:43 +0000

    ✨ Initial day 10

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -22,22 +22,65 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day10 (
-    -- day10a
-  -- , day10b
+    day10a
+  , day10b
   ) where
 
 import           AOC.Prelude
 
+isOpen = (`elem` "({[<")
+
+matching = \case
+  "()" -> True
+  "[]" -> True
+  "{}" -> True
+  "<>" -> True
+  _    -> False
+
+score = \case
+    ')' -> Sum 3
+    ']' -> Sum 57
+    '}' -> Sum 1197
+    '>' -> Sum 25137
+    _   -> Sum 0
+
+scoreb = \case
+    '(' -> 1
+    '[' -> 2
+    '{' -> 3
+    '<' -> 4
+    _   -> 0
+
+solve [] = Nothing
+solve (b:bs) = solve' bs [b]
+    where
+        solve' [] _ = Nothing
+        solve' (_:_) [] = Nothing
+        solve' (b':bs') (o:os) | isOpen b'        = solve' bs' (b':o:os)
+                               | matching [o, b'] = solve' bs' os
+                               | otherwise        = Just $ score b'
+
+solveb [] = Nothing
+solveb (b:bs) = solve' bs [b]
+    where
+        solve' _ [] = Nothing
+        solve' [] os = Just $ foldl (\s c -> (s*5) + scoreb c) 0 os
+        solve' (b':bs') (o:os) | isOpen b'        = solve' bs' (b':o:os)
+                               | matching [o, b'] = solve' bs' os
+                               | otherwise        = Nothing
+
+middle ns = ns !? (length ns `div` 2)
+
 day10a :: _ :~> _
 day10a = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = fmap getSum . foldMap solve
     }
 
 day10b :: _ :~> _
 day10b = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = middle . sort . mapMaybe solveb
     }
