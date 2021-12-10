ec20e5f50aebbddb2068081d10e65c32dd3a5eb2
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2021-12-10 23:16:07 +0000

    ♻️ Clean up day 10

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
 -- |
 -- Module      : AOC.Challenge.Day10
@@ -28,8 +29,19 @@ module AOC.Challenge.Day10 (
 
 import           AOC.Prelude
 
+data AutocompleteScore a = (Num a) => Auto { autoScore :: a }
+
+instance Semigroup (AutocompleteScore a) where
+    (Auto s) <> (Auto s') = Auto ((s * 5) + s')
+
+instance (Num a) => Monoid (AutocompleteScore a) where
+    mempty = Auto 0
+    mconcat = foldl (<>) mempty
+
+isOpen :: Char -> Bool
 isOpen = (`elem` "({[<")
 
+matching :: String -> Bool
 matching = \case
   "()" -> True
   "[]" -> True
@@ -37,50 +49,56 @@ matching = \case
   "<>" -> True
   _    -> False
 
-score = \case
+scorea :: Char -> Sum Integer
+scorea = \case
     ')' -> Sum 3
     ']' -> Sum 57
     '}' -> Sum 1197
     '>' -> Sum 25137
-    _   -> Sum 0
+    _   -> mempty
 
+scoreb :: Char -> AutocompleteScore Integer
 scoreb = \case
-    '(' -> 1
-    '[' -> 2
-    '{' -> 3
-    '<' -> 4
-    _   -> 0
+    '(' -> Auto 1
+    '[' -> Auto 2
+    '{' -> Auto 3
+    '<' -> Auto 4
+    _   -> mempty
 
-solve [] = Nothing
-solve (b:bs) = solve' bs [b]
+
+solvea :: String -> Maybe (Sum Integer)
+solvea [] = Nothing
+solvea (b:bs) = solve' bs [b]
     where
         solve' [] _ = Nothing
         solve' (_:_) [] = Nothing
         solve' (b':bs') (o:os) | isOpen b'        = solve' bs' (b':o:os)
                                | matching [o, b'] = solve' bs' os
-                               | otherwise        = Just $ score b'
+                               | otherwise        = Just $ scorea b'
 
+solveb :: String -> Maybe Integer
 solveb [] = Nothing
 solveb (b:bs) = solve' bs [b]
     where
         solve' _ [] = Nothing
-        solve' [] os = Just $ foldl (\s c -> (s*5) + scoreb c) 0 os
+        solve' [] os = Just . autoScore . mconcat . map scoreb $ os
         solve' (b':bs') (o:os) | isOpen b'        = solve' bs' (b':o:os)
                                | matching [o, b'] = solve' bs' os
                                | otherwise        = Nothing
 
-middle ns = ns !? (length ns `div` 2)
+middle :: (Ord a) => [a] -> Maybe a
+middle ns = sort ns !? (length ns `div` 2)
 
-day10a :: _ :~> _
+day10a :: [String] :~> Integer
 day10a = MkSol
     { sParse = Just . lines
     , sShow  = show
-    , sSolve = fmap getSum . foldMap solve
+    , sSolve = fmap getSum . foldMap solvea
     }
 
-day10b :: _ :~> _
+day10b :: [String] :~> Integer
 day10b = MkSol
     { sParse = Just . lines
     , sShow  = show
-    , sSolve = middle . sort . mapMaybe solveb
+    , sSolve = middle . mapMaybe solveb
     }
