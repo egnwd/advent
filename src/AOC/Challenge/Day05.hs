590c6ba2bf52ef40b061efeefa0e2383ab2d3d13
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2021-12-30 17:01:38 +0000

    💩 Slow day 5

--- src/AOC/Challenge/Day05.hs
+++ src/AOC/Challenge/Day05.hs
@@ -22,22 +22,56 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day05 (
-    -- day05a
-  -- , day05b
+    day05a
+  , day05b
+                           , encodeWord8
   ) where
 
 import           AOC.Prelude
+import Crypto.Hash.MD5
+import Numeric.Lens
+import Control.Lens
+import Data.Bits
+import Data.Word
+import Data.ByteString as B (unpack)
+import Data.ByteString.UTF8 as BS (ByteString, fromString)
+import Data.ByteString.Char8 as BC (pack, unpack, index)
+import qualified Data.IntMap as M
+
+encodeWord8 :: Word8 -> [Word]
+encodeWord8 x = map fromIntegral [ (x .&. 0xF0) `shiftR` 4, x .&. 0xF ]
+
+meetsCondition :: Int -> BS.ByteString -> Bool
+meetsCondition n s = all (== 0) (take half bytes) && (even n || bytes !! half < 16)
+  where half = n `div` 2
+        bytes = B.unpack s
+
+findPassword nth zs s = (!? nth) . iterate (loopEither go) $ ([], 0)
+    where
+      go (pw, n) = let hsh = hash (s <> (pack . show $ n))
+                    in if meetsCondition zs hsh
+                          then Left ((encodeWord8 <=< B.unpack) hsh !! zs : pw, n+1)
+                          else Right (pw, n+1)
+
+findPasswordSecure nth zs s = (!? nth) . iterate (loopEither go) $ (M.empty, 0)
+    where
+      go (pw, n) = let hsh = hash (s <> (pack . show $ n))
+                       pos = (fmap fromIntegral . encodeWord8 <=< B.unpack) hsh !! zs
+                       c = (encodeWord8 <=< B.unpack) hsh !! (zs+1)
+                    in if meetsCondition zs hsh && pos < nth && pos `M.notMember` pw
+                          then Left (M.insert pos c pw, n+1)
+                          else Right (pw, n+1)
 
 day05a :: _ :~> _
 day05a = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = Just . BS.fromString
+    , sShow  = toListOf (traverse . re hex . traverse)
+    , sSolve = fmap (reverse . fst) . findPassword (dyno_ "nth" 8) (dyno_ "zeros" 5)
     }
 
 day05b :: _ :~> _
 day05b = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = Just . BS.fromString
+    , sShow  = toListOf (traverse . re hex . traverse)
+    , sSolve = fmap (M.elems . fst) . findPasswordSecure (dyno_ "nth" 8) (dyno_ "zeros" 5)
     }
