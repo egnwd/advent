fd02a78f1f93c5e937f85ce6a11efc34653d3ff9
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-01-01 16:13:17 +0000

    ♻️ Clean up day 5

--- src/AOC/Challenge/Day05.hs
+++ src/AOC/Challenge/Day05.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day05
 -- License     : BSD3
@@ -8,23 +5,11 @@
 -- Stability   : experimental
 -- Portability : non-portable
 --
--- Day 5.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
+-- Day 5.
 
 module AOC.Challenge.Day05 (
     day05a
   , day05b
-                           , encodeWord8
   ) where
 
 import           AOC.Prelude
@@ -35,7 +20,7 @@ import Data.Bits
 import Data.Word
 import Data.ByteString as B (unpack)
 import Data.ByteString.UTF8 as BS (ByteString, fromString)
-import Data.ByteString.Char8 as BC (pack, unpack, index)
+import Data.ByteString.Char8 as BC (pack)
 import qualified Data.IntMap as M
 
 encodeWord8 :: Word8 -> [Word]
@@ -46,14 +31,15 @@ meetsCondition n s = all (== 0) (take half bytes) && (even n || bytes !! half < 
   where half = n `div` 2
         bytes = B.unpack s
 
-findPassword nth zs s = (!? nth) . iterate (loopEither go) $ ([], 0)
+findPassword :: Int -> Int -> ByteString -> Maybe ([Word], Int)
+findPassword nth zs s = iterate (loopEither go) ([], 0) !? nth
     where
       go (pw, n) = let hsh = hash (s <> (pack . show $ n))
                     in if meetsCondition zs hsh
                           then Left ((encodeWord8 <=< B.unpack) hsh !! zs : pw, n+1)
                           else Right (pw, n+1)
-
-findPasswordSecure nth zs s = (!? nth) . iterate (loopEither go) $ (M.empty, 0)
+findPasswordSecure :: Int -> Int -> ByteString -> Maybe (IntMap Word, Int)
+findPasswordSecure nth zs s = iterate (loopEither go) (M.empty, 0) !? nth
     where
       go (pw, n) = let hsh = hash (s <> (pack . show $ n))
                        pos = (fmap fromIntegral . encodeWord8 <=< B.unpack) hsh !! zs
@@ -62,14 +48,14 @@ findPasswordSecure nth zs s = (!? nth) . iterate (loopEither go) $ (M.empty, 0)
                           then Left (M.insert pos c pw, n+1)
                           else Right (pw, n+1)
 
-day05a :: _ :~> _
+day05a :: ByteString :~> [Word]
 day05a = MkSol
     { sParse = Just . BS.fromString
     , sShow  = toListOf (traverse . re hex . traverse)
     , sSolve = fmap (reverse . fst) . findPassword (dyno_ "nth" 8) (dyno_ "zeros" 5)
     }
 
-day05b :: _ :~> _
+day05b :: ByteString :~> [Word]
 day05b = MkSol
     { sParse = Just . BS.fromString
     , sShow  = toListOf (traverse . re hex . traverse)
