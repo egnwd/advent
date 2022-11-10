90e788e9461cfa2f9edef99aba12f8157a6026e6
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-11-10 19:27:11 +0000

    ✨ Finished Day 4

--- src/AOC/Challenge/Day04.hs
+++ src/AOC/Challenge/Day04.hs
@@ -22,22 +22,60 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day04 (
-    -- day04a
-  -- , day04b
+    day04a
+  , day04b
   ) where
 
 import           AOC.Prelude
+import Data.Maybe
+import Control.Lens
+
+twoConsecutiveNumbers :: String -> Maybe String
+twoConsecutiveNumbers p = go p
+    where
+        go :: String -> Maybe String
+        go [] = Nothing
+        go [_] = Nothing
+        go (a:b:bs)
+          | a == b = Just p
+          | otherwise = go (b:bs)
+
+monotonicIncrease :: String -> Maybe String
+monotonicIncrease p = go p
+    where
+        go :: String -> Maybe String
+        go [] = Just p
+        go [_] = Just p
+        go (a:b:bs)
+            | digitToInt a <= digitToInt b = go (b:bs)
+            | otherwise = Nothing
+
+alonePair p | any (==2) . map length . group $ p = Just p
+            | otherwise = Nothing
+
+combineRules :: [String -> Maybe String] -> String -> Maybe String
+combineRules rs i = foldM (&) i rs
+
+solvea mn mx = countTrue (isJust . passwordRule . show) [mn..mx]
+    where
+        rules = [twoConsecutiveNumbers, monotonicIncrease]
+        passwordRule = combineRules rules
+
+solveb mn mx = countTrue (isJust . passwordRule . show) [mn..mx]
+    where
+        rules = [alonePair, monotonicIncrease]
+        passwordRule = combineRules rules
 
 day04a :: _ :~> _
 day04a = MkSol
-    { sParse = Just
+    { sParse = traverseOf both (readMaybe @ Int . filter isDigit) . splitAt 6
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . uncurry solvea
     }
 
 day04b :: _ :~> _
 day04b = MkSol
-    { sParse = Just
+    { sParse = traverseOf both (readMaybe @ Int . filter isDigit) . splitAt 6
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . uncurry solveb
     }
