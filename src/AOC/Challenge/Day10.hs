d019d854ab6896d891056f415cf31049f1f29676
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-11-14 21:01:33 +0000

    🚧 Work in progress Day 10

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -22,22 +22,38 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day10 (
-    -- day10a
-  -- , day10b
+    day10a
+  , day10b
   ) where
 
 import           AOC.Prelude
+import           Linear
+import qualified Data.Set as S
+
+angleTo :: Point -> Point -> Float
+angleTo p q = atan (fromIntegral dy / fromIntegral dx)
+    where
+        V2 dy dx = p - q
+
+inSight :: Set Point -> Point -> [Point]
+inSight as p = filter go . S.toList . S.delete p $ as
+    where
+        go q = all (`S.notMember` as) . tail . init $ lineTo (V2 p q)
+
+solvea as = getMax . foldMap (Max . length . inSight as) $ as
+
+findWinningBet as l = V2 8 2
 
 day10a :: _ :~> _
 day10a = MkSol
-    { sParse = Just
+    { sParse = Just . parseAsciiSet (== '#')
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solvea
     }
 
 day10b :: _ :~> _
 day10b = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = Just . parseAsciiSet (=='#')
+    , sShow  = \(V2 x y) -> show $ x * 100 + y
+    , sSolve = \as -> Just . findWinningBet as . solvea $ as
     }
