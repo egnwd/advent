5da8fe74a9ced51d2c09d970816d2d4470f99b01
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-11-20 22:24:59 +0000

    ✨ Really slow day 10

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- |
 -- Module      : AOC.Challenge.Day10
@@ -22,22 +23,42 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day10 (
-    -- day10a
-  -- , day10b
+    day10a
+  , day10b
   ) where
 
-import           AOC.Prelude
+import           AOC.Prelude hiding (Point)
+import Linear (V2(..), _x, _y)
+import qualified Linear as L
+import Control.Lens
+import Advent.OCR
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+parseStar :: (Num a) => CharParser (V2 a, V2 a)
+parseStar = do
+    pos <- V2 <$> (pTok "position=<" *> pDecimal <* pTok ",") <*> (pDecimal <* pTok ">")
+    vel <- V2 <$> (pTok "velocity=<" *> pDecimal <* pTok ",") <*> (pDecimal <* pTok ">")
+    return (pos, vel)
+
+step (ps, vs) = (zipWith (+) ps vs, vs)
+
+-- Very slow
+run t !x = let x' = step x
+            in case parseLettersWith (view _x) (view _y) . S.fromList . fst $ x' of
+                 Just str -> (t+1, str)
+                 Nothing -> run (t+1) x'
 
 day10a :: _ :~> _
 day10a = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = fmap unzip . parseLines parseStar
+    , sShow  = id
+    , sSolve = Just . snd . run 0
     }
 
 day10b :: _ :~> _
 day10b = MkSol
-    { sParse = Just
+    { sParse = fmap unzip . parseLines parseStar
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . fst . run 0
     }
