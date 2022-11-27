d170a4dfd842b24434fd2c09868dd42cfa0c1681
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-11-27 18:36:05 +0000

    ✨ Work in progress but 12 a

--- src/AOC/Challenge/Day12.hs
+++ src/AOC/Challenge/Day12.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      : AOC.Challenge.Day12
 -- License     : BSD3
@@ -22,22 +24,63 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day12 (
-    -- day12a
-  -- , day12b
+    day12a
+  , day12b
   ) where
 
-import           AOC.Prelude
+import           AOC.Prelude hiding (many)
+import qualified Data.IntMap as M
+import qualified Data.Set as S
+import Text.Megaparsec.Char
+import Control.Monad.Combinators
+
+parseInitialState :: CharParser (Set Int)
+parseInitialState = S.fromList . map fst . filter snd . zip [0..] <$> (pTok "initial state:" *> many (True <$ char '#' <|> False <$ char '.') <* newline)
+
+parseInput :: CharParser (Set Int, Set String)
+parseInput = do
+    s0 <- parseInitialState
+    newline
+    ons <- S.fromList . lefts <$> (mappingParser `sepBy` newline)
+    return (s0, ons)
+
+mappingParser = do
+    nh <- pWord <* pTok "=>"
+    side <- (Left <$ char '#') <|> (Right <$ char '.')
+    return $ side nh
+
+step :: Set String -> Set Int -> Set Int
+step from state = S.fromList state'
+    where
+        mn = S.findMin state
+        mx = S.findMax state
+        state' = filter (keepFrom from state) [mn-2..mx+2]
+
+keepFrom from state i = S.member p from
+    where
+        toState True = '#'
+        toState False = '.'
+        p = map (toState . (`S.member` state)) [i-2..i+2]
+
+indexedSeenBefore :: Ord a => (a -> a) -> a -> (Int, a)
+indexedSeenBefore f x = go 1 (S.singleton x) x
+  where
+    go idx !seen !x
+        | y `S.member` seen = (idx, y)
+        | otherwise = go (idx+1) (S.insert y seen) y
+      where
+        y = f x
 
 day12a :: _ :~> _
 day12a = MkSol
-    { sParse = Just
+    { sParse = parseMaybeLenient parseInput
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(s0, changes) -> Just . sum . head . drop (dyno_ "steps" 20) . iterate (step changes) $ s0
     }
 
 day12b :: _ :~> _
 day12b = MkSol
-    { sParse = Just
+    { sParse = parseMaybeLenient parseInput
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(s0, changes) -> Just . indexedSeenBefore (step changes) $ s0
     }
