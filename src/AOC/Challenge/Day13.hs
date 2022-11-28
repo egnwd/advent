c146f3972857d14fd36431b8d795c98b47735896
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-11-28 09:17:39 +0000

    ✨ Day 13 parsing

--- src/AOC/Challenge/Day13.hs
+++ src/AOC/Challenge/Day13.hs
@@ -22,15 +22,48 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day13 (
-    -- day13a
-  -- , day13b
+    day13a
+  , day13b
   ) where
 
 import           AOC.Prelude
+import AOC.Common.Point
+import Control.Arrow ((&&&))
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+data Intersection = I { exits :: Set Dir } deriving Show
+
+parseIntersection x = x <$ (guard (x /= ' '))
+
+toCar = \case
+ '>' -> pure East
+ '<' -> pure West
+ '^' -> pure North
+ 'v' -> pure South
+ _ -> Nothing
+
+justCars = M.mapMaybe toCar
+
+justIntersections :: Map Point Char -> Map Point _
+justIntersections tracks = tracks'
+    where
+        tracks' = M.mapMaybe id . M.mapWithKey toIntersection $ tracks
+        toIntersection loc c = do
+            guard (c /= ' ')
+            let es = M.fromList . mapMaybe (sequence . second (flip M.lookup tracks . (+loc) . dirVec)) $ zip [North ..] [North ..]
+            case (c, M.lookup North es) of
+              ('/', Just '|')  -> return $ I (S.fromList [North, West])
+              ('/', _)         -> return $ I (S.fromList [South, East])
+              ('\\', Just '|') -> return $ I (S.fromList [North, East])
+              ('\\', _)        -> return $ I (S.fromList [South, West])
+              ('+', _)         -> return $ I (S.fromList [South, West, North, East])
+              (_, _)           -> Nothing
+
 
 day13a :: _ :~> _
 day13a = MkSol
-    { sParse = Just
+    { sParse = Just . (justIntersections &&& justCars) . parseAsciiMap parseIntersection
     , sShow  = show
     , sSolve = Just
     }
