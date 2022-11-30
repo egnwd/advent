a81aedcaa8a51e973c1ad0dc8a20eeab371fdf5e
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-11-30 19:15:31 +0000

    🚧 Day 14 in progress

--- src/AOC/Challenge/Day14.hs
+++ src/AOC/Challenge/Day14.hs
@@ -22,15 +22,35 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day14 (
-    -- day14a
-  -- , day14b
+    day14a
+  , day14b
   ) where
 
 import           AOC.Prelude
+import qualified Data.Sequence as S
+import qualified Data.List.PointedList as PL
+import qualified Data.List.PointedList.Circular as PLC
+import Data.List.PointedList.Circular (PointedList)
 
-day14a :: _ :~> _
+type Recipes = PointedList Int
+type Elves = (Int, Int)
+
+newRecipes = S.unfoldl (\x -> x `divMod` 10 <$ guard (x > 0))
+
+starterRecipes :: Recipes
+starterRecipes = fromJust $ PLC.fromList [3,7]
+
+round :: (Elves, Recipes) -> Maybe (Elves, Recipes)
+round ((e1,e2), rs) = undefined
+--    score1 <- preview PL.focus =<< PL.moveTo e1 rs
+--    score2 <- preview PL.focus =<< PL.moveTo e2 rs
+--    rsEnd <- PL.moveTo ((PL.length rs) - 1) rs
+--    let rs' = foldl (\r n -> PL.insert n r) (newRecipes (score1 + score2))
+--    return ((e1',e2'), rs')
+
+day14a :: Int :~> _
 day14a = MkSol
-    { sParse = Just
+    { sParse = readMaybe
     , sShow  = show
     , sSolve = Just
     }
