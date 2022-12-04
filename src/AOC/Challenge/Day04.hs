dedff4dd0c8811380b069e2e59d9a372d9b0f464
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-12-04 05:17:16 +0000

    ✨ Day 4 two stars

--- src/AOC/Challenge/Day04.hs
+++ src/AOC/Challenge/Day04.hs
@@ -22,22 +22,48 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day04 (
-    -- day04a
-  -- , day04b
+    day04a
+  , day04b
   ) where
 
 import           AOC.Prelude
+import Linear
+import qualified Data.Ix as Ix
+
+import qualified Data.Graph.Inductive           as G
+import qualified Data.IntMap                    as IM
+import qualified Data.IntSet                    as IS
+import qualified Data.List.NonEmpty             as NE
+import qualified Data.List.PointedList          as PL
+import qualified Data.List.PointedList.Circular as PLC
+import qualified Data.Map                       as M
+import qualified Data.OrdPSQ                    as PSQ
+import qualified Data.Sequence                  as Seq
+import qualified Data.Set                       as S
+import qualified Data.Text                      as T
+import qualified Data.Vector                    as V
+import qualified Linear                         as L
+import qualified Text.Megaparsec                as P
+import qualified Text.Megaparsec.Char           as P
+import qualified Text.Megaparsec.Char.Lexer     as PP
+
+parseThing :: String -> Maybe (S.Set Int, S.Set Int)
+parseThing s = do
+    [a,b,c,d] <- traverse readMaybe . words . clearOut (not . isDigit) $ s
+    let l = S.fromList . Ix.range $ (a,b)
+    let r = S.fromList . Ix.range $ (c,d)
+    return (l,r)
 
 day04a :: _ :~> _
 day04a = MkSol
-    { sParse = Just
+    { sParse = traverse parseThing . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . countTrue (\(l,r) -> S.isSubsetOf l r || S.isSubsetOf r l)
     }
 
 day04b :: _ :~> _
 day04b = MkSol
-    { sParse = Just
+    { sParse = traverse parseThing . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . countTrue (\(l,r) -> (>0) . S.size $ S.intersection l r)
     }
