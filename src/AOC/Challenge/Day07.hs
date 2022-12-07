6cb5ea59fb769d69caff0e3d90fdf2ed11271333
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-12-07 09:04:39 +0000

    ✨ Day 7 two stars

--- src/AOC/Challenge/Day07.hs
+++ src/AOC/Challenge/Day07.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
+{-# LANGUAGE OverloadedStrings, TypeFamilies #-}
 -- |
 -- Module      : AOC.Challenge.Day07
 -- License     : BSD3
@@ -22,8 +22,9 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day07 (
-    -- day07a
-  -- , day07b
+    day07a
+  , day07b
+                           , totalSize, example
   ) where
 
 import           AOC.Prelude
@@ -44,17 +45,98 @@ import qualified Linear                         as L
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
+import qualified Data.Functor.Foldable                    as F
+import Data.Functor.Foldable.TH (makeBaseFunctor)
+import Data.String
+import Control.Lens
+
+data Entry = File String Int | Dir String [Entry] deriving (Eq, Ord, Show)
+
+$(makeBaseFunctor ''Entry)
+
+data Command = CD String | LS deriving (Eq, Ord, Show)
+
+data Instruction = C Command | E Entry deriving (Eq, Ord, Show)
+
+example :: Entry
+example = Dir "/"
+  [ Dir "a" [ Dir "e" [ File "i" 584 ]
+            , File "f" 29116
+            , File "g" 2557
+            , File "h.lst" 62596
+            ]
+  , File "b.txt" 14848514
+  , File "c.dat" 8504156
+  , Dir "d" [ File "j" 4060174
+            , File "d.log" 8033020
+            , File "d.ext" 5626152
+            , File "k" 7214296
+            ]
+  ]
+
+totalSize :: Entry -> Int
+totalSize = sum . filter (< 100000) . snd . F.para go
+    where
+        go :: EntryF (Entry, (Int, [Int])) -> (Int, [Int])
+        go (FileF n sz) = (sz, [])
+        go (DirF n sz) = let sub = concatMap (\(e, (z,s)) -> case e of Dir _ _ -> z:s; _ -> []) $ sz
+                             me = sum . map (fst . snd) $ sz
+                          in (me, sub)
+
+        getDir (File _ _) = (*0)
+        getDir (Dir _ _) = id
+
+        getFile (File _ _) = id
+        getFile (Dir _ _) = (*0)
+
+totalSpace = 70000000
+need = 30000000
+
+deleteSmallest :: Entry -> Int
+deleteSmallest e = let (total, sizes) = F.para go e
+                       needToFree = need - (totalSpace - total)
+                    in minimum . filter (>= needToFree) $ sizes
+    where
+        go :: EntryF (Entry, (Int, [Int])) -> (Int, [Int])
+        go (FileF n sz) = (sz, [])
+        go (DirF n sz) = let sub = concatMap (\(e, (z,s)) -> case e of Dir _ _ -> z:s; _ -> []) $ sz
+                             me = sum . map (fst . snd) $ sz
+                          in (me, sub)
+
+        getDir (File _ _) = (*0)
+        getDir (Dir _ _) = id
+
+        getFile (File _ _) = id
+        getFile (Dir _ _) = (*0)
+
+parseStructure :: CharParser Entry
+parseStructure = do
+    P.try (P.notFollowedBy ("$ cd .." <* P.newline))
+    n <- pTok "$ cd" *> pWord <* P.newline
+    "$ ls" <* P.newline
+    files <- parseFiles
+    dirs <- P.many parseStructure
+    optional ("$ cd .." <* P.newline)
+    return $ Dir n (files ++ dirs)
+    where
+        parseFiles = P.sepEndBy (P.try (P.skipManyTill parseDirectory parseFile)) P.newline <* P.skipMany parseDirectory
+        parseDirectory = pTok "dir" *> pWord <* P.newline
+        parseFile :: CharParser Entry
+        parseFile = flip File <$> (pTok pDecimal) <*> pWord
+
+prettyDir (FileF n sz) = "- " ++ n ++ " (file, size=" ++ show sz ++ ")"
+prettyDir (DirF n es) = "- " ++ n ++ " (dir)\n" ++ (intercalate "\n" . map (intercalate "\n" . map ("  " ++) . lines) $ es)
 
 day07a :: _ :~> _
 day07a = MkSol
-    { sParse = Just
+    { sParse = Just . parseOrFail (parseStructure <* P.eof)
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . totalSize
     }
 
 day07b :: _ :~> _
 day07b = MkSol
-    { sParse = Just
+    { sParse = Just . parseOrFail (parseStructure <* P.eof)
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . deleteSmallest
     }
