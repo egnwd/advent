e163daa13e692883b0b0d4e903b1a49ab0cd36e4
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-12-13 00:40:21 +0000

    ✨ Day 10 two stars

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      : AOC.Challenge.Day10
 -- License     : BSD3
@@ -22,8 +24,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day10 (
-    -- day10a
-  -- , day10b
+    day10a
+  , day10b
   ) where
 
 import           AOC.Prelude
@@ -44,17 +46,116 @@ import qualified Linear                         as L
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
+import           Control.DeepSeq     (NFData)
+import           Control.Lens
+import           Control.Monad.State
+import           Data.Conduino
+import           Data.Conduino.Lift
+import           Control.Exception
+import           Control.Lens.TH
+import           Control.Monad.Error.Lens
+import           Control.Monad.Except
+import           Data.Typeable
+import qualified Data.Conduino.Combinators as C
+import           GHC.Generics        (Generic)
+import Advent.OCR
+
+data Instruction = Noop | AddX Int deriving (Eq, Show)
+
+parseInstruction = (Noop <$ "noop") <|> (AddX <$> ("addx " *> pDecimal))
+
+data WalkieTalkie = WT
+ { wtRegX :: Int
+ , wtCycles :: Int
+ } deriving (Eq, Ord, Show, Generic)
+instance NFData WalkieTalkie
+
+_wtRegX :: Lens' WalkieTalkie Int
+_wtRegX = lens wtRegX (\wt newX -> wt { wtRegX = newX })
+
+_wtCycles :: Lens' WalkieTalkie Int
+_wtCycles = lens wtCycles (\wt newC -> wt { wtCycles = newC })
+
+class Monad m => MonadWalkie m where
+    wtCycle  :: m ()
+    wtRead   :: m Int
+    wtWrite  :: Int -> m ()
+    wtSignal :: m (Int, Int)
+
+instance Monad m => MonadWalkie (StateT WalkieTalkie m) where
+    wtRead = gets wtRegX
+    wtWrite x = _wtRegX += x
+    wtCycle = _wtCycles += 1
+    wtSignal = do
+        WT{..} <- get
+        return $ (wtCycles, wtRegX)
+
+instance MonadWalkie m => MonadWalkie (Pipe i o u m) where
+    wtRead = lift wtRead
+    wtWrite = lift . wtWrite
+    wtCycle = lift wtCycle
+    wtSignal = lift wtSignal
+
+step :: MonadWalkie m => Instruction -> Pipe i _ u m ()
+step i = do
+    case i of
+      Noop -> wtCycle *> wtSignal >>= yield
+      AddX x -> do
+          wtCycle
+          wtSignal >>= yield
+          wtCycle
+          wtSignal >>= yield
+          wtWrite x
+
+stepTilTermination :: Monad m => [Instruction] -> Pipe i _ u m WalkieTalkie
+stepTilTermination s = execStateP (WT 1 0) (traverse_ step s)
+
+interestingSignals :: Monad m => Pipe _ o u m Int
+interestingSignals
+  = (C.drop 19 >> C.map id)
+  .| (do
+      C.take 1 -- 20
+      C.drop 39
+      C.take 1 -- 60
+      C.drop 39
+      C.take 1 -- 100
+      C.drop 39
+      C.take 1 -- 140
+      C.drop 39
+      C.take 1 -- 180
+      C.drop 39
+      C.take 1) -- 220
+  .| C.foldl (+) 0
+
+run :: [Instruction] -> Int
+run s = runPipePure $ stepTilTermination s .| C.map (uncurry (*)) .| interestingSignals
+
+drawPixel :: (Int, Int) -> (Point, Char)
+drawPixel (c, x) = (loc c, p)
+    where
+        loc = uncurry (flip L.V2) . (`divMod` 40) . subtract 1
+        pos = (c-1) `mod` 40
+        p = if pos == (x-1) || pos == x || pos == (x+1)
+               then '#'
+               else '.'
+
+runb :: [Instruction] -> _
+runb s = runPipePure
+    $ stepTilTermination s
+    .| C.map drawPixel
+    .| C.take 240
+    .| C.sinkList
 
 day10a :: _ :~> _
 day10a = MkSol
-    { sParse = Just
+    { sParse = parseLines parseInstruction
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . run
     }
 
 day10b :: _ :~> _
 day10b = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = parseLines parseInstruction
+    , sShow  = maybe "UNKNOWN" id . asciiMapToLetters (S.singleton '#') . displayAsciiMap '.'
+    , sSolve = Just . M.fromList . runb
     }
