e5bfd273792819e43a35ab8420d622c13bb121ea
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-12-16 12:14:03 +0000

    ✨ Day 15 two stars

--- src/AOC/Challenge/Day15.hs
+++ src/AOC/Challenge/Day15.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      : AOC.Challenge.Day15
 -- License     : BSD3
@@ -22,11 +24,23 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day15 (
-    -- day15a
-  -- , day15b
+    day15a
+  , day15b
+                           , remove
+                           , splitInterval
   ) where
 
 import           AOC.Prelude
+
+import Linear
+
+import Control.Lens
+import Control.Arrow
+import Data.Ix
+
+import           Data.Interval ((<=..<=))
+import Data.IntervalRelation
+import qualified Data.Interval as I
 
 import qualified Data.Graph.Inductive           as G
 import qualified Data.IntMap                    as IM
@@ -45,16 +59,66 @@ import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
 
+parseSensor = do
+    -- Sensor at x=2300471, y=2016823: closest beacon is at x=2687171, y=2822745
+    pTok "Sensor at"
+    x <- "x=" *> pDecimal <* pTok (P.char ',')
+    y <- "y=" *> pDecimal <* pTok (P.char ':')
+    return $ V2 x y
+
+parseBeacon = do
+    pTok "closest beacon is at"
+    x <- "x=" *> pDecimal <* pTok (P.char ',')
+    y <- "y=" *> pDecimal
+    return $ V2 x y
+
+findLocationsWithoutBeaconInRow :: Int -> _ -> _
+findLocationsWithoutBeaconInRow r locs = beam - holes - beacons - sensors
+    where
+        beam = succ . I.width . I.hulls $ ranges
+        holes = sum . map (succ . I.width) . foldl' remove [I.hulls ranges] $ ranges
+        beacons = countTrue ((==r) . view _y) . S.fromList . map snd $ locs
+        sensors = countTrue ((==r) . view _y) . S.fromList . map fst $ locs
+        ranges = filter (not . I.null) . map (uncurry rowRange) $ locs
+        rowRange :: Point -> Point -> I.Interval Int
+        rowRange s@(V2 sx sy) b@(V2 bx by)
+          = let offset = manhattan s b - abs (sy - r)
+             in if offset < 0 then I.empty else (I.Finite (sx-offset) <=..<= I.Finite (sx+offset))
+
+findHolesInRow :: Int -> _ -> _
+findHolesInRow r locs = mapMaybe I.extractSingleton holes
+    where
+        holes = foldl' remove [I.hulls ranges] $ ranges
+        ranges = filter (not . I.null) . map (uncurry rowRange) $ locs
+        rowRange :: Point -> Point -> I.Interval Int
+        rowRange s@(V2 sx sy) b@(V2 bx by)
+          = let offset = manhattan s b - abs (sy - r)
+             in if offset < 0 then I.empty else (I.Finite (sx-offset) <=..<= I.Finite (sx+offset))
+
+remove is i = concatMap (filter (not . I.null) . splitInterval i) is
+splitInterval i i'
+  = case i `I.relate` i' of
+       Overlaps -> [I.interval (I.upperBound i + 1, I.Closed) (I.upperBound' i')]
+       Starts -> [I.interval (I.upperBound i + 1, I.Closed) (I.upperBound' i')]
+       During -> [I.interval (I.lowerBound' i') (I.lowerBound i - 1, I.Closed), I.interval (I.upperBound i + 1, I.Closed) (I.upperBound' i')]
+       Finishes -> [I.interval (I.lowerBound' i') (I.lowerBound i - 1, I.Closed)]
+       Equal -> []
+       FinishedBy -> []
+       Contains -> []
+       StartedBy -> []
+       OverlappedBy -> [I.interval (I.lowerBound' i') (I.lowerBound i - 1, I.Closed)]
+       _ -> [i'] -- not connected
+
 day15a :: _ :~> _
 day15a = MkSol
-    { sParse = Just
+    { sParse = parseLines ((,) <$> parseSensor <*> parseBeacon)
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . findLocationsWithoutBeaconInRow (dyno_ "key" 2000000)
     }
 
 day15b :: _ :~> _
 day15b = MkSol
-    { sParse = Just
+    { sParse = parseLines ((,) <$> parseSensor <*> parseBeacon)
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \locs -> fmap (\b -> 4000000 *  (b ^. _y) + (b ^. _x)) . preview _head . concatMap (\r -> map (V2 r) . findHolesInRow r $ locs) $ [0..(dyno_ "mxy" 4000000)]
     }
