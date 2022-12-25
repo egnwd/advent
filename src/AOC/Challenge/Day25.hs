b6737a3a8eb0be2b11bbf15f4421b8efb5dd7559
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2022-12-25 08:00:00 +0000

    ✨ Christmas Day

--- src/AOC/Challenge/Day25.hs
+++ src/AOC/Challenge/Day25.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
+{-# LANGUAGE TypeFamilies #-}
+
 -- |
 -- Module      : AOC.Challenge.Day25
 -- License     : BSD3
@@ -22,11 +24,16 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day25 (
-    -- day25a
-  -- , day25b
+    day25a
+  , day25b
+                           , toNum
+                           , fromNum'
+                           , toSnafu
   ) where
 
 import           AOC.Prelude
+
+import Data.Functor.Foldable
 
 import qualified Data.Graph.Inductive           as G
 import qualified Data.IntMap                    as IM
@@ -44,12 +51,77 @@ import qualified Linear                         as L
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
+import qualified Data.Functor.Foldable.TH       as F
 
+data SNAFU
+ = Z        -- bottom
+ | O SNAFU  -- 0
+ | I SNAFU  -- 1
+ | II SNAFU -- 2
+ | M SNAFU  -- -
+ | MM SNAFU -- =
+ deriving (Eq, Ord)
+
+$(F.makeBaseFunctor ''SNAFU)
+
+instance Show SNAFU where
+    show  Z     = ""
+    show (O  s) = show s ++ "0"
+    show (I  s) = show s ++ "1"
+    show (II s) = show s ++ "2"
+    show (M  s) = show s ++ "-"
+    show (MM s) = show s ++ "="
+
+toSnafu :: String -> Maybe SNAFU
+toSnafu = foldl' (\n a -> go a <*> n) (Just Z)
+    where
+        go :: Char -> Maybe (SNAFU -> SNAFU)
+        go '0' = Just O
+        go '1' = Just I
+        go '2' = Just II
+        go '-' = Just M
+        go '=' = Just MM
+        go  _  = Nothing
+
+sumSnafu :: [SNAFU] -> SNAFU
+sumSnafu = fromNum' . sum . map toNum
+
+toNum :: SNAFU -> Natural
+toNum = cata go
+    where
+        go :: SNAFUF Natural -> Natural
+        go = \case
+            ZF -> 0
+            (OF n) -> n * 5
+            (IF n) -> (n * 5) + 1
+            (IIF n) -> (n * 5) + 2
+            (MF n) -> (n * 5) - 1
+            (MMF n) -> (n * 5) - 2
+
+foldl f a bs = foldr (\b g x -> g (f x b)) id bs a
+
+fromNum' n = fromNum n Z
+
+fromNum :: Natural -> SNAFU -> SNAFU
+fromNum n = case n `quotRem` 5 of
+              (0, 0) -> O
+              (0, 1) -> I
+              (0, 2) -> II
+              (0, 3) -> MM . I
+              (0, 4) -> M . I
+              (q, 0) -> O . fromNum q
+              (q, 1) -> I . fromNum q
+              (q, 2) -> II . fromNum q
+              (_, 3) -> MM . fromNum next
+              (_, 4) -> M . fromNum next
+              _ -> undefined
+          where
+              next = (n + 5 - 1) `div` 5
 day25a :: _ :~> _
 day25a = MkSol
-    { sParse = Just
+    { sParse = traverse toSnafu . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sumSnafu
     }
 
 day25b :: _ :~> _
