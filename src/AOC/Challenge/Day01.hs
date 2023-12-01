46152b22b769df96b01386a7cd095d06510ec60a
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2023-12-01 09:19:14 +0000

    ♻️ Use ViewPatterns to make it more legible

--- src/AOC/Challenge/Day01.hs
+++ src/AOC/Challenge/Day01.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 -- |
 -- Module      : AOC.Challenge.Day01
 -- License     : BSD3
@@ -15,49 +17,48 @@ module AOC.Challenge.Day01 (
 
 import AOC.Solver    ((:~>)(..))
 import Control.Monad (guard)
-import Data.Char     (isNumber)
+import Data.Char     (isNumber, digitToInt)
 import Data.Functor  (($>))
-import Data.List     (tails)
+import Data.List     (tails, isPrefixOf)
 import Data.Maybe    (mapMaybe)
-import Text.Read     (readMaybe)
 
-topAndTail :: [a] -> [a]
-topAndTail xs = [head xs, last xs]
+topAndTail :: [Int] -> Int
+topAndTail xs = head xs * 10 + last xs
+
+pattern One, Two, Three, Four, Five, Six, Seven, Eight, Nine :: String
+pattern One   <- (isPrefixOf "one"   -> True)
+pattern Two   <- (isPrefixOf "two"   -> True)
+pattern Three <- (isPrefixOf "three" -> True)
+pattern Four  <- (isPrefixOf "four"  -> True)
+pattern Five  <- (isPrefixOf "five"  -> True)
+pattern Six   <- (isPrefixOf "six"   -> True)
+pattern Seven <- (isPrefixOf "seven" -> True)
+pattern Eight <- (isPrefixOf "eight" -> True)
+pattern Nine  <- (isPrefixOf "nine"  -> True)
+
+numberMap :: String -> Maybe Int
+numberMap One     = Just 1
+numberMap Two     = Just 2
+numberMap Three   = Just 3
+numberMap Four    = Just 4
+numberMap Five    = Just 5
+numberMap Six     = Just 6
+numberMap Seven   = Just 7
+numberMap Eight   = Just 8
+numberMap Nine    = Just 9
+numberMap (x : _) = guard (isNumber x) $> digitToInt x
+numberMap []      = Nothing
 
 day01a :: [String] :~> Int
 day01a = MkSol
     { sParse = Just . lines
     , sShow  = show
-    , sSolve = fmap sum . traverse (readMaybe . topAndTail . filter isNumber)
+    , sSolve = Just . sum . map (topAndTail . map digitToInt . filter isNumber)
     }
-
-pattern One, Two, Three, Four, Five, Six, Seven, Eight, Nine :: String
-pattern One <- ('o':'n':'e': _)
-pattern Two <- ('t':'w':'o' : _)
-pattern Three <- ('t':'h':'r':'e':'e' : _)
-pattern Four <- ('f':'o':'u':'r'  : _)
-pattern Five <- ('f':'i':'v':'e'  : _)
-pattern Six <- ('s':'i':'x'   : _)
-pattern Seven <- ('s':'e':'v':'e':'n' : _)
-pattern Eight <- ('e':'i':'g':'h':'t' : _)
-pattern Nine <- ('n':'i':'n':'e'  : _)
-
-numberMap :: String -> Maybe Char
-numberMap One     = Just '1'
-numberMap Two     = Just '2'
-numberMap Three   = Just '3'
-numberMap Four    = Just '4'
-numberMap Five    = Just '5'
-numberMap Six     = Just '6'
-numberMap Seven   = Just '7'
-numberMap Eight   = Just '8'
-numberMap Nine    = Just '9'
-numberMap (x : _) = guard (isNumber x) $> x
-numberMap []      = Nothing
 
 day01b :: [String] :~> Int
 day01b = MkSol
     { sParse = Just . lines
     , sShow  = show
-    , sSolve = fmap sum . traverse (readMaybe . topAndTail . mapMaybe numberMap . tails)
+    , sSolve = Just . sum . map (topAndTail . mapMaybe numberMap . tails)
     }
