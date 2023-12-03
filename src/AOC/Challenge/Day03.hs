6f3345749e3d6b27a4c7a99825ac54c3ec398022
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2023-12-03 11:24:01 +0000

    ♻️ Tidy up types

--- src/AOC/Challenge/Day03.hs
+++ src/AOC/Challenge/Day03.hs
@@ -17,7 +17,7 @@ import           AOC.Common    (parseMaybeLenient, parseAsciiMap, Point, allNeig
 import           Control.Monad (guard)
 import           Data.Maybe    (mapMaybe)
 import           Data.Char     (isNumber)
-import           Control.Lens  (makePrisms, sumOf)
+import           Control.Lens  (makePrisms, sumOf, toListOf)
 import           Linear        (V2(..))
 import           Control.Arrow ((&&&))
 import           Data.Map      (Map)
@@ -25,20 +25,18 @@ import qualified Data.Map                       as M
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
 
-type PartLocation = ([Point], Int)
+data Part = Part [Point] Int
 data Gear = PartialGear Int | FullGear Int
 makePrisms ''Gear
 
-parseNumberMap :: String -> (Map Point Char, [PartLocation])
+parseNumberMap :: String -> (Map Point Char, [Part])
 parseNumberMap = parseAsciiMap symbolGetter &&& go
     where
         symbolGetter '.' = Nothing
         symbolGetter a
           | isNumber a = Nothing
           | otherwise = Just a
-        go = concat
-           . concat
-           . sequenceA
+        go = toListOf (traverse . traverse . traverse)
            . zipWith (parseMaybeLenient . parseLine) [0..]
            . lines
         parseLine y = P.many $ P.try $ do
@@ -46,33 +44,33 @@ parseNumberMap = parseAsciiMap symbolGetter &&& go
             start <- P.unPos . P.sourceColumn <$> P.getSourcePos
             n <- PP.decimal
             end <- P.unPos . P.sourceColumn <$> P.getSourcePos
-            return ([V2 (x-1) y | x <- [start..end-1]], n)
+            return $ Part [V2 (x-1) y | x <- [start..end-1]] n
 
 nearSymbols :: (Char -> Bool) -> Map Point Char -> [Point] -> Map Point Char
 nearSymbols p symbols = M.restrictKeys (M.filter p symbols) . foldMap allNeighboursSet
 
-findPartNumbers :: Map Point Char -> [PartLocation] -> [Int]
-findPartNumbers symbolMap = mapMaybe (\(k, a) -> a <$ guard (hasSymbolNearby k))
+findPartNumbers :: Map Point Char -> [Part] -> [Int]
+findPartNumbers symbolMap = mapMaybe (\(Part k a) -> a <$ guard (hasSymbolNearby k))
     where
         hasSymbolNearby = not . M.null . nearSymbols (const True) symbolMap
 
-findGearRatios :: Map Point Char -> [PartLocation] -> Map Point Gear
+findGearRatios :: Map Point Char -> [Part] -> Map Point Gear
 findGearRatios symbolMap = foldl partToGear M.empty
     where
-        partToGear mp (k,a) = foldr (M.alter (tweakGear a)) mp . nearbyGearPositions $ k
+        partToGear mp (Part k a) = foldr (M.alter (tweakGear a)) mp . nearbyGearPositions $ k
         nearbyGearPositions = M.keys . nearSymbols (== '*') symbolMap
         tweakGear a Nothing = Just . PartialGear $ a
         tweakGear a (Just (PartialGear b)) = Just . FullGear $ a * b
         tweakGear _ _ = Nothing
 
-day03a :: (Map Point Char, [PartLocation]) :~> Int
+day03a :: (Map Point Char, [Part]) :~> Int
 day03a = MkSol
     { sParse = Just . parseNumberMap
     , sShow  = show
     , sSolve = Just . sum . uncurry findPartNumbers
     }
 
-day03b :: (Map Point Char, [PartLocation]) :~> Int
+day03b :: (Map Point Char, [Part]) :~> Int
 day03b = MkSol
     { sParse = Just . parseNumberMap
     , sShow  = show
