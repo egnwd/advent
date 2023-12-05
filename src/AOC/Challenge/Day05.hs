96dbec14c5a3d0493fb7bbfbbe5f0a0579c5402f
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2023-12-05 19:45:28 +0000

    ✨ Messy Day 5

--- src/AOC/Challenge/Day05.hs
+++ src/AOC/Challenge/Day05.hs
@@ -22,8 +22,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day05 (
-    -- day05a
-  -- , day05b
+    day05a
+  , day05b
   ) where
 
 import           AOC.Prelude
@@ -44,17 +44,51 @@ import qualified Linear                         as L
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
+import Control.Lens
+import Data.IntervalMap.Strict (IntervalMap)
+import qualified Data.Interval as IV
+import qualified Data.IntervalMap.Strict as IVMap
 
-day05a :: _ :~> _
+mapToNext (dst,src,rng) x
+  | src <= x && x <= (src+rng) = Just $ x + (dst - src)
+  | otherwise                  = Nothing
+
+listTup3 [a,b,c] = Just (a,b,c)
+listTup3 _ = Nothing
+
+parseData (s:mps) = sequenceTuple (seeds, mps')
+    where
+        seeds = traverse (readMaybe @ Int) . tail . words $ s
+        mps' = traverse (traverse (listTup3 <=< traverse (readMaybe @ Int) . words) . tail . lines) mps
+
+getNext :: [(Int, Int, Int)] -> Int -> Int
+getNext mp = fromMaybe <$> id <*> asum . sequence (map mapToNext mp)
+
+getNexts :: [(Int, Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+getNexts mp xs = go <> (map hack . IVMap.keys $ tmp IVMap.\\ mp')
+    where
+        hack :: IV.Interval Int -> _
+        hack i = let (IV.Finite a) = IV.lowerBound i
+                     (IV.Finite b) = IV.upperBound i
+                  in (a,b)
+        mp' = IVMap.fromList . map (\(a,b,c) -> (makeRange b (b + c), a - b)) $ mp
+        makeRange a b = IV.Finite a IV.<=..<= IV.Finite b
+        tmp = IVMap.fromList . map (,()) . map (uncurry makeRange) $ xs
+        go = map (hack . (\(i,d) -> IV.mapMonotonic (+d) i)) . IVMap.toList $ IVMap.intersectionWith const mp' tmp
+
+pairs [] = []
+pairs (x:y:rest) = (x,x+y) : pairs rest
+
+day05a :: ([Int], [[(Int, Int, Int)]]) :~> _
 day05a = MkSol
-    { sParse = Just
+    { sParse = parseData . splitOn "\n\n"
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(s, mps) -> minimumOf traverse . map (\x -> foldl' (flip getNext) x mps) $ s
     }
 
 day05b :: _ :~> _
 day05b = MkSol
-    { sParse = Just
+    { sParse = parseData . splitOn "\n\n"
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(s, mps) -> minimumOf (traverse . traverse . _1) . map (\x -> foldl (flip getNexts) [x] mps) . pairs $ s
     }
