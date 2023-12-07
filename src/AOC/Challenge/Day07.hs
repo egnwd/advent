c4a853f7eca1c40ef418b88d0c5890a37abdfd11
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2023-12-07 06:50:45 +0000

    ✨ Messy Day 7

--- src/AOC/Challenge/Day07.hs
+++ src/AOC/Challenge/Day07.hs
@@ -22,8 +22,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day07 (
-    -- day07a
-  -- , day07b
+    day07a
+  , day07b
   ) where
 
 import           AOC.Prelude
@@ -37,7 +37,7 @@ import qualified Data.List.PointedList.Circular as PLC
 import qualified Data.Map                       as M
 import qualified Data.OrdPSQ                    as PSQ
 import qualified Data.Sequence                  as Seq
-import qualified Data.Set                       as S
+import qualified Data.Set.NonEmpty              as S
 import qualified Data.Text                      as T
 import qualified Data.Vector                    as V
 import qualified Linear                         as L
@@ -45,16 +45,97 @@ import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
 
+data Hands = FiveOf | FourOf | FullHouse | ThreeOf | TwoPair | Pair | HighCard deriving (Eq, Ord, Show)
+
+data Label = A | K | Q | T | N9 | N8 | N7 | N6 | N5 | N4 | N3 | N2 | J deriving (Ord, Eq)
+
+readLabel :: Char -> Label
+readLabel = \case
+    'A' -> A
+    'K' -> K
+    'Q' -> Q
+    'J' -> J
+    'T' -> T
+    '9' -> N9
+    '8' -> N8
+    '7' -> N7
+    '6' -> N6
+    '5' -> N5
+    '4' -> N4
+    '3' -> N3
+    '2' -> N2
+
+compares :: [a -> a -> Ordering] -> a -> a -> Ordering
+compares cs x y = mconcat $ map (\c -> c x y) cs
+
+lookupInFreq = IM.lookup
+
+hand :: [Char] -> Hands
+hand (revFreq->cs)
+  | isJust . lookupInFreq 5 $ cs = FiveOf
+  | isJust . lookupInFreq 4 $ cs = FourOf
+  | (isJust . lookupInFreq 2 $ cs) && (isJust . lookupInFreq 3 $ cs) = FullHouse
+  | (isJust . lookupInFreq 3 $ cs) = ThreeOf
+  | maybe False ((== 2) . S.size) . lookupInFreq 2 $ cs = TwoPair
+  | isJust . lookupInFreq 2 $ cs = Pair
+  | otherwise = HighCard
+
+jokerHand :: [Char] -> Hands
+jokerHand cs = flip upgrade (freqs cs) . jokerHand' $ revFreq (filter (/='J') cs)
+    where
+        jokerHand' cs
+          | isJust . lookupInFreq 5 $ cs = FiveOf
+          | isJust . lookupInFreq 4 $ cs = FourOf
+          | (isJust . lookupInFreq 2 $ cs) && (isJust . lookupInFreq 3 $ cs) = FullHouse
+          | (isJust . lookupInFreq 3 $ cs) = ThreeOf
+          | maybe False ((== 2) . S.size) . lookupInFreq 2 $ cs = TwoPair
+          | isJust . lookupInFreq 2 $ cs = Pair
+          | otherwise = HighCard
+
+        upgrade FiveOf rs = FiveOf
+        upgrade FourOf rs
+          | lookupFreq 'J' rs == 1 = FiveOf
+          | otherwise = FourOf
+        upgrade FullHouse rs = FullHouse
+        upgrade ThreeOf rs
+          | lookupFreq 'J' rs == 1 = FourOf
+          | lookupFreq 'J' rs == 2 = FiveOf
+          | otherwise = ThreeOf
+        upgrade TwoPair rs
+          | lookupFreq 'J' rs == 1 = FullHouse
+          | otherwise = TwoPair
+        upgrade Pair rs
+          | lookupFreq 'J' rs == 1 = ThreeOf
+          | lookupFreq 'J' rs == 2 = FourOf
+          | lookupFreq 'J' rs == 3 = FiveOf
+          | otherwise = Pair
+        upgrade HighCard rs
+          | lookupFreq 'J' rs == 1 = Pair
+          | lookupFreq 'J' rs == 2 = ThreeOf
+          | lookupFreq 'J' rs == 3 = FourOf
+          | lookupFreq 'J' rs >= 4 = FiveOf
+          | otherwise = HighCard
+
+
+highCard :: [Char] -> [Label]
+highCard = map readLabel
+
+rank :: [([Char], Int)] -> [([Char], Int)]
+rank xs = sortBy (flip $ compares [comparing (hand . fst), comparing (highCard . fst)]) xs
+
+jokerRank :: [([Char], Int)] -> [([Char], Int)]
+jokerRank xs = sortBy (flip $ compares [comparing (jokerHand . fst), comparing (highCard . fst)]) xs
+
 day07a :: _ :~> _
 day07a = MkSol
-    { sParse = Just
+    { sParse = traverse ((sequence . second (readMaybe @ Int)) <=< listTup . words) . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . zipWith (\i (_, b) -> i * b) [1..] . rank
     }
 
 day07b :: _ :~> _
 day07b = MkSol
-    { sParse = Just
+    { sParse = sParse day07a
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . zipWith (\i (_, b) -> i * b) [1..] . jokerRank
     }
