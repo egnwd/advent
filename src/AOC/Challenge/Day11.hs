a8b29339b9a311ad634752cd3a53ddefbda3f947
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2023-12-11 22:45:50 +0000

    ✨ Day 11 messy

--- src/AOC/Challenge/Day11.hs
+++ src/AOC/Challenge/Day11.hs
@@ -22,8 +22,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day11 (
-    -- day11a
-  -- , day11b
+    day11a
+  , day11b
   ) where
 
 import           AOC.Prelude
@@ -38,23 +38,38 @@ import qualified Data.Map                       as M
 import qualified Data.OrdPSQ                    as PSQ
 import qualified Data.Sequence                  as Seq
 import qualified Data.Set                       as S
+import qualified Data.Set.NonEmpty              as NES
 import qualified Data.Text                      as T
 import qualified Data.Vector                    as V
 import qualified Linear                         as L
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
+import Linear
+import Control.Lens
+
+solve d gs = sum $ zipWith (\x ys -> sum [manhattan x y | y <- ys]) newGalaxies (tail . tails $ newGalaxies)
+    where
+        Just (V2 (V2 xmn ymn) (V2 xmx ymx)) = boundingBox <$> NES.nonEmptySet gs
+        xs = S.map (view _x) gs
+        ys = S.map (view _y) gs
+        V2 exs eys = fmap (filter (/= 0)) . sequence $ extraRows ++ extraColumns
+        newGalaxies = S.toList $
+            foldr' (\y -> S.mapMonotonic (\g -> if (g ^. _y) > y then g & _y %~ (+(d-1)) else g))
+            (foldr' (\x -> S.mapMonotonic (\g -> if (g ^. _x) > x then g & _x %~ (+(d-1)) else g)) gs exs) eys
+        extraColumns = [V2 0 y | y <- [ymn .. ymx], y `S.notMember` ys]
+        extraRows = [V2 x 0 | x <- [xmn .. xmx], x `S.notMember` xs]
 
 day11a :: _ :~> _
 day11a = MkSol
-    { sParse = Just
+    { sParse = Just . parseAsciiSet (=='#')
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve 2
     }
 
 day11b :: _ :~> _
 day11b = MkSol
-    { sParse = Just
+    { sParse = Just . parseAsciiSet (=='#')
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve (dyno_ "d" 1000000)
     }
