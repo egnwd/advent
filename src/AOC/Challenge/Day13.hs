338e0cb48a5639cf5d161b1870834ee1234f1921
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2023-12-13 22:06:22 +0000

    ✨ Messy day 13

--- src/AOC/Challenge/Day13.hs
+++ src/AOC/Challenge/Day13.hs
@@ -22,8 +22,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day13 (
-    -- day13a
-  -- , day13b
+    day13a
+  , day13b
   ) where
 
 import           AOC.Prelude
@@ -38,23 +38,44 @@ import qualified Data.Map                       as M
 import qualified Data.OrdPSQ                    as PSQ
 import qualified Data.Sequence                  as Seq
 import qualified Data.Set                       as S
+import qualified Data.Set.NonEmpty              as NES
 import qualified Data.Text                      as T
 import qualified Data.Vector                    as V
 import qualified Linear                         as L
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
+import Control.Lens
+
+offBy a b = S.size $ (a S.\\ b) `S.union` (b S.\\ a)
+
+compareRocks :: (Int, Set Int) -> (Int, Set Int) -> Maybe (Int, Int)
+compareRocks (r,rc) (r', rc')
+  | 1 >= offBy rc rc' = Just (r,r')
+  | otherwise = Nothing
+
+isReflection n mp = (n ==) . sum . mapMaybe (\(l, r) -> offBy <$> (M.lookup l mp) <*> (M.lookup r mp))
+
+search n xy mn mx r0 = do
+    let strips = M.mapKeysWith S.union (^. (L._yx . xy)) . M.fromSet (S.singleton . (^. xy)) $ r0
+    let f (l,r) = guard (isReflection n strips (zip [l,(l-1) .. mn] [r .. mx])) $> l + 1
+    asum $ zipWith (\a -> f <=< compareRocks a) (M.toList strips) (tail $ M.toList strips)
+
+findReflection :: Int -> Set Point -> Maybe _
+findReflection n r0 = do
+    (L.V2 (L.V2 xmn ymn) (L.V2 xmx  ymx)) <- boundingBox <$> NES.nonEmptySet r0
+    (flip L.V2 0 <$> search n L._x ymn ymx r0) <|> (L.V2 0 <$> search n L._y xmn xmx r0)
 
 day13a :: _ :~> _
 day13a = MkSol
-    { sParse = Just
+    { sParse = Just . map (parseAsciiSet (=='#')) . splitOn "\n\n"
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . (\(L.V2 x y) -> x * 100 + y) . sum . mapMaybe (findReflection 0)
     }
 
 day13b :: _ :~> _
 day13b = MkSol
-    { sParse = Just
+    { sParse = sParse day13a
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . (\(L.V2 x y) -> x * 100 + y) . sum . mapMaybe (findReflection 1)
     }
