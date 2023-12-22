1f9922ba74bf8c7837db800501f9a7b53673545a
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2023-12-22 13:27:41 +0000

    ✨ Day 18 solved

--- src/AOC/Challenge/Day18.hs
+++ src/AOC/Challenge/Day18.hs
@@ -22,8 +22,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day18 (
-    -- day18a
-  -- , day18b
+    day18a
+  , day18b
   ) where
 
 import           AOC.Prelude
@@ -38,6 +38,7 @@ import qualified Data.Map                       as M
 import qualified Data.OrdPSQ                    as PSQ
 import qualified Data.Sequence                  as Seq
 import qualified Data.Set                       as S
+import qualified Data.Set.NonEmpty              as NES
 import qualified Data.Text                      as T
 import qualified Data.Vector                    as V
 import qualified Linear                         as L
@@ -45,16 +46,46 @@ import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
 
+import Linear (V2(..))
+
+data Plan = Plan Dir Int deriving (Eq, Show)
+
+parseLineA = do
+    d <- pTok $ P.choice $ zipWith (\a b -> a <$ P.char b) [North ..] "URDL"
+    n <- pTok pDecimal
+    _ <- P.between (P.char '(') (P.char ')') (P.char '#' *> P.takeWhileP Nothing isAlphaNum)
+    return $ Plan d n
+
+parseLineB = do
+    _ <- pWord <* pWord
+    n <- foldl' (\acc x -> acc * 16 + digitToInt x) 0 <$> (P.string "(#" *> P.takeP Nothing 5)
+    d <- P.choice $ zipWith (\a b -> a <$ P.char b) [North ..] "3012"
+    return $ Plan d n
+
+dig :: [Plan] -> NE.NonEmpty Point
+dig = snd . foldl' go (0, 0 NE.:| [])
+    where
+        go (start, trench) (Plan dir n) = let end :: Point = start + (n L.*^ dirVec dir)
+                                           in (end, end NE.<| trench)
+
+excavate (t NE.:| ts) = dugOut + trenchTunnel
+    where
+        -- shoelace seems to take into account one side of the trench...
+        trenchTunnel = succ . (`div` 2 ) . sum $ zipWith (((sum . abs) .) . subtract) (t : ts) ts
+        -- https://en.wikipedia.org/wiki/Shoelace_formula
+        dugOut = (`div` 2) . abs . sum $ zipWith go (t : ts) (ts ++ [t])
+        go (V2 x y) (V2 x' y') = x * y' - y * x'
+
 day18a :: _ :~> _
 day18a = MkSol
-    { sParse = Just
+    { sParse = parseLines parseLineA
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . excavate . dig
     }
 
 day18b :: _ :~> _
 day18b = MkSol
-    { sParse = Just
+    { sParse = parseLines parseLineB
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . excavate . dig
     }
