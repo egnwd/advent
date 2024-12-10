3d5524297cc09b1a83b320bc464b3698264431a1
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2024-12-10 21:36:32 +0000

    refactor :recycle: Tidy up

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -13,15 +13,15 @@ module AOC.Challenge.Day10 (
   , day10b
   ) where
 
-import           AOC.Solver ((:~>)(..))
-import           AOC.Common (parseAsciiMap, decDigit, Point, neighboursSet)
+import           AOC.Solver     ((:~>)(..))
+import           AOC.Common     (parseAsciiMap, decDigit, Point, neighboursSet)
 
-import           Data.Map                       (Map)
-import qualified Data.Map                       as M
-import qualified Data.Set                       as S
-import Data.Finite (Finite)
-import Control.Lens (preview)
-import Data.Semigroup (Sum(..))
+import           Control.Lens   (preview)
+import           Data.Finite    (Finite)
+import           Data.Map       (Map)
+import           Data.Semigroup (Sum(..))
+import qualified Data.Map       as M
+import qualified Data.Set       as S
 
 next :: Map Point (Finite 10) -> Finite 10 -> Point -> [Point]
 next mp a p = M.keys . M.filter (== succ a) $ mp `M.restrictKeys` neighboursSet  p
@@ -33,16 +33,15 @@ searchTrailheads found score mp = getSum . foldMap (score . buildTrail 0) $ M.ke
           | x == 9 = found p
           | otherwise = foldMap (buildTrail (succ x)) $ next mp x p
 
-day10a :: Map Point (Finite 10) :~> Int
-day10a = MkSol
+day10 :: (Monoid a, Num b, Show b) => (Point -> a) -> (a -> Sum b) -> Map Point (Finite 10) :~> b
+day10 found score = MkSol
     { sParse = Just . parseAsciiMap (preview decDigit)
     , sShow  = show
-    , sSolve = Just . searchTrailheads S.singleton (Sum . S.size)
+    , sSolve = Just . searchTrailheads found score
     }
 
+day10a :: Map Point (Finite 10) :~> Int
+day10a = day10 S.singleton (Sum . S.size)
+
 day10b :: Map Point (Finite 10) :~> Int
-day10b = MkSol
-    { sParse = Just . parseAsciiMap (preview decDigit)
-    , sShow  = show
-    , sSolve = Just . searchTrailheads (const 1) id
-    }
+day10b = day10 (const 1) id
