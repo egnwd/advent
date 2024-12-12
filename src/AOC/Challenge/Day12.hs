383cea8d5716b0b1347ed93c02a4040f4dccf88f
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2024-12-12 06:01:44 +0000

    feat: :sparkles: Raw day 12

--- src/AOC/Challenge/Day12.hs
+++ src/AOC/Challenge/Day12.hs
@@ -22,8 +22,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day12 (
-    -- day12a
-  -- , day12b
+    day12a
+  , day12b
   ) where
 
 import           AOC.Prelude
@@ -38,6 +38,8 @@ import qualified Data.Map                       as M
 import qualified Data.OrdPSQ                    as PSQ
 import qualified Data.Sequence                  as Seq
 import qualified Data.Set                       as S
+import           Data.Set.NonEmpty              (NESet)
+import qualified Data.Set.NonEmpty              as NES
 import qualified Data.Text                      as T
 import qualified Data.Vector                    as V
 import qualified Linear                         as L
@@ -45,16 +47,35 @@ import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
 
+splitMaps :: Map Point Char -> Map Char (Set Point)
+splitMaps = M.fromListWith S.union . map (\(a,b) -> (b, S.singleton a)) . M.toList
+
+price :: Set (NESet Point) -> Int
+price = sum . map (\x -> perim x * area x) . S.toList
+    where
+        perim :: NESet Point -> Int
+        perim (NES.toSet->x) = sum . map (S.size . (`S.difference` x) . neighboursSet) . S.toList $ x
+        area = NES.size
+
+priceDiscount :: Set (NESet Point) -> Int
+priceDiscount = sum . map (\x -> perim x * area x) . S.toList
+    where
+        perim :: NESet Point -> Int
+        perim (NES.toSet->x) = sum $ map go [ North .. ]
+            where
+                go d = S.size . contiguousRegions . foldMap ((`S.difference` x) . S.singleton . (+dirVec d)) $ x
+        area = NES.size
+
 day12a :: _ :~> _
 day12a = MkSol
-    { sParse = Just
+    { sParse = Just . parseAsciiMap Just
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . M.map (price . contiguousRegions) . splitMaps
     }
 
 day12b :: _ :~> _
 day12b = MkSol
-    { sParse = Just
+    { sParse = sParse day12a
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . M.map (priceDiscount . contiguousRegions) . splitMaps
     }
