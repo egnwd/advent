dc5911c252030d022886af2796e8a9f3813cffc3
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2024-12-23 16:22:29 +0000

    feat: Day 23

--- src/AOC/Challenge/Day23.hs
+++ src/AOC/Challenge/Day23.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day23
 -- License     : BSD3
@@ -10,51 +7,60 @@
 --
 -- Day 23.  See "AOC.Solver" for the types used in this module!
 --
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day23 (
-    -- day23a
-  -- , day23b
+    day23a
+  , day23b
   ) where
 
 import           AOC.Prelude
 
-import qualified Data.Graph.Inductive           as G
-import qualified Data.IntMap                    as IM
-import qualified Data.IntSet                    as IS
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.List.PointedList          as PL
-import qualified Data.List.PointedList.Circular as PLC
 import qualified Data.Map                       as M
-import qualified Data.OrdPSQ                    as PSQ
-import qualified Data.Sequence                  as Seq
 import qualified Data.Set                       as S
-import qualified Data.Text                      as T
-import qualified Data.Vector                    as V
-import qualified Linear                         as L
-import qualified Text.Megaparsec                as P
-import qualified Text.Megaparsec.Char           as P
-import qualified Text.Megaparsec.Char.Lexer     as PP
 
-day23a :: _ :~> _
+parse :: String -> Maybe (Map String (Set String))
+parse s = do
+    ts <- traverse (listTup . splitOn "-") . lines $ s
+    let mp = M.fromListWith (<>) . map (second S.singleton) $ ts
+    let mp' = M.fromListWith (<>) . map (swap . first S.singleton) $ ts
+    return $ M.unionWith (<>) mp mp'
+
+party :: Set String -> Bool
+party = not . S.null . S.filter ((== Just 't') . listToMaybe)
+
+findTricycle :: Map String (Set String) -> _
+findTricycle mp = S.size . S.filter party . fold . snd $ M.mapAccumWithKey (\seen k _ -> (S.insert k seen, go 3 k seen)) S.empty mp
+    where
+        expand :: String -> Set String
+        expand x = mp M.! x
+        go :: Int -> String -> Set String -> Set (Set String)
+        go n0 x0 seen0 = dfs (n0-1) x0 seen0 S.empty
+            where
+                dfs 0 x _ path
+                  | x0 `S.member` expand x = S.singleton $ S.insert x path
+                  | otherwise = mempty
+                dfs n x seen path = foldl' (\a b -> a <> dfs (n-1) b (S.insert x seen) (S.insert x path)) S.empty (expand x `S.difference` seen)
+
+findMaximumClique :: Map String (Set String) -> _
+findMaximumClique  = S.toAscList . maximumBy (compare `on` S.size) . go
+    where
+        go :: Map String (Set String) -> Set (Set String)
+        go g = case M.minViewWithKey g of
+                 Nothing -> S.singleton S.empty
+                 Just ((v, ns), g') -> S.foldr' (\k -> S.insert (S.insert v (k `S.intersection` ns)) . S.insert k) S.empty (go g')
+
+
+
+day23a :: Map String (Set String) :~> Int
 day23a = MkSol
-    { sParse = Just
+    { sParse = parse
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . findTricycle
     }
 
-day23b :: _ :~> _
+day23b :: Map String (Set String) :~> [String]
 day23b = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = parse
+    , sShow  = intercalate ","
+    , sSolve = Just . findMaximumClique
     }
