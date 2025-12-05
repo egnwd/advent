3cd7746f9ce871723e8722bca943f440522013aa
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2025-12-05 08:37:29 -0500

    feat: :sparkles: Day 4

--- src/AOC/Challenge/Day04.hs
+++ src/AOC/Challenge/Day04.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day04
 -- License     : BSD3
@@ -9,52 +6,49 @@
 -- Portability : non-portable
 --
 -- Day 4.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day04 (
-    -- day04a
-  -- , day04b
+    day04a
+  , day04b
   ) where
 
-import           AOC.Prelude
+import           AOC.Common (parseAsciiSet, allNeighboursSet, fixedPoint, countTrue, Point)
+import           AOC.Solver  ((:~>)(..))
 
-import qualified Data.Graph.Inductive           as G
-import qualified Data.IntMap                    as IM
-import qualified Data.IntSet                    as IS
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.List.PointedList          as PL
-import qualified Data.List.PointedList.Circular as PLC
+import           Data.Map                       (Map)
 import qualified Data.Map                       as M
-import qualified Data.OrdPSQ                    as PSQ
-import qualified Data.Sequence                  as Seq
+import           Data.Set                       (Set)
 import qualified Data.Set                       as S
-import qualified Data.Text                      as T
-import qualified Data.Vector                    as V
-import qualified Linear                         as L
-import qualified Text.Megaparsec                as P
-import qualified Text.Megaparsec.Char           as P
-import qualified Text.Megaparsec.Char.Lexer     as PP
+import           Data.List                      (foldl')
+
+has4Adjacent :: Map Point (Set Point) -> Point -> Bool
+has4Adjacent ps p = (<4) . S.size $ ps M.! p
+
+getNeighbours :: Set Point -> Map Point (Set Point)
+getNeighbours ps = M.map (S.intersection ps) $ M.fromSet allNeighboursSet ps
+
+removeRoll :: Map Point (Set Point) -> Point -> Map Point (Set Point)
+removeRoll neighs p = foldl' (flip (M.adjust (S.delete p))) (M.delete p neighs) (neighs M.! p)
+
+part2 :: Set Point -> Set Point
+part2 ps0 = M.keysSet $ fixedPoint go ns
+    where
+        ns = getNeighbours ps0
+        go :: Map Point (Set Point) -> Map Point (Set Point)
+        go ps = foldl' removeRoll ps toRemove
+            where
+                toRemove = filter (has4Adjacent ps) $ M.keys ps
 
 day04a :: _ :~> _
 day04a = MkSol
-    { sParse = Just . lines
+    { sParse = Just . parseAsciiSet (=='@')
     , sShow  = show
-    , sSolve = Just . id
+    , sSolve = \ps -> Just $ countTrue (has4Adjacent (getNeighbours ps)) ps
     }
 
 day04b :: _ :~> _
 day04b = MkSol
     { sParse = sParse day04a
     , sShow  = show
-    , sSolve = Just . id
+    , sSolve =  \ps0 -> Just . S.size . (ps0 `S.difference`) $ part2 ps0
     }
